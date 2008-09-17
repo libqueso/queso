e340d36f914f292f84fc34991d8ee0de0ca6fbe4
Ernesto Prudencio <prudenci@ices.utexas.edu>
2008-09-17 15:16:26 +0000

    backup

--- examples/mcmc/tga/uqTgaEx2.h
+++ examples/mcmc/tga/uqTgaEx2.h
@@ -41,8 +41,8 @@ int func(double t, const double Mass[], double f[], void *info)
 }
 
 //********************************************************
-// Likelihood function object for one UQ problem stage (with prefix "stage0_").
-// A likelihood function object is provided by user and is called by the MCMC library.
+// Likelihood function object for one validation problem stage (with prefix "stage0_").
+// A likelihood function object is provided by user and is called by the UQ library.
 // This likelihood function object consists of data and routine.
 //********************************************************
 
@@ -96,7 +96,7 @@ void stage0_likelihoodRoutine(const P_V& paramValues, const void* functionDataPt
                         paramValues.env().rank(),
                         "stage0_likelihoodRoutine()",
                         "gsl_odeiv_evolve_apply() failed");
-    //printf("%6.1lf %10.4lf\n",t,Mass[0]);
+    //printf("t = %6.1lf, mass = %10.4lf\n",t,Mass[0]);
     //loopSize++;
 		
     while ( (t_old <= Te1[i]) && (Te1[i] <= t) ) {
@@ -113,7 +113,7 @@ void stage0_likelihoodRoutine(const P_V& paramValues, const void* functionDataPt
 	
   //printf("loopSize = %d\n",loopSize);
   if ((paramValues.env().verbosity() >= 10) && (paramValues.env().rank() == 0)) {
-    printf("For A = %g, E = %g, and beta1 = %.3lf, misfit1 = %lf and likelihood1 = %lf.\n",A,E,beta1,misfit1,resultValues[0]);
+    printf("In stage0_likelihoodRoutine(), A = %g, E = %g, beta1 = %.3lf: misfit1 = %lf, likelihood1 = %lf.\n",A,E,beta1,misfit1,resultValues[0]);
   }
 
   gsl_odeiv_evolve_free (e);
@@ -124,8 +124,8 @@ void stage0_likelihoodRoutine(const P_V& paramValues, const void* functionDataPt
 }
 
 //********************************************************
-// QoI function object for one UQ problem stage (with prefix "stage0_").
-// A QoI function object is provided by user and is called by the MCMC library.
+// QoI function object for one validation problem stage (with prefix "stage0_").
+// A QoI function object is provided by user and is called by the UQ library.
 // This QoI function object consists of data and routine.
 //********************************************************
 // The (user defined) data type for the data needed by the (user defined) qoi routine
@@ -134,19 +134,72 @@ struct
 stage0_qoiRoutine_DataType
 {
   double beta1;
+  double criticalMass1;
 };
 
 // The actual (user defined) qoi routine
 template<class P_V,class P_M,class S_V,class S_M,class Q_V,class Q_M>
 void stage0_qoiRoutine(const P_V& paramValues, const void* functionDataPtr, Q_V& qoiValues)
 {
-  qoiValues[0] = 2.;
+  double A             = paramValues[0];
+  double E             = paramValues[1];
+  double beta1         = ((stage0_qoiRoutine_DataType<S_V,S_M> *) functionDataPtr)->beta1;
+  double criticalMass1 = ((stage0_qoiRoutine_DataType<S_V,S_M> *) functionDataPtr)->criticalMass1;
+
+  double params[]={A,E,beta1};
+      	
+  // integration
+  const gsl_odeiv_step_type *T   = gsl_odeiv_step_rkf45; //rkf45; //gear1;
+        gsl_odeiv_step      *s   = gsl_odeiv_step_alloc(T,1);
+        gsl_odeiv_control   *c   = gsl_odeiv_control_y_new(1e-6,0.0);
+        gsl_odeiv_evolve    *e   = gsl_odeiv_evolve_alloc(1);
+        gsl_odeiv_system     sys = {func, NULL, 1, (void *)params}; 
+	
+  double t = 0.1, t1 = 800.;
+  double h = 1e-3;
+  double Mass[1];
+  Mass[0]=1.;
+  
+  double t_old = 0.;
+  double M_old[1];
+  M_old[0]=1.;
+	
+  double crossingTemperature = 0.;
+  //unsigned int loopSize = 0;
+  while ((t       < t1           ) &&
+         (Mass[0] > criticalMass1)) {
+    int status = gsl_odeiv_evolve_apply(e, c, s, &sys, &t, t1, &h, Mass);
+    UQ_FATAL_TEST_MACRO((status != GSL_SUCCESS),
+                        paramValues.env().rank(),
+                        "stage0_qoiRoutine()",
+                        "gsl_odeiv_evolve_apply() failed");
+    //printf("t = %6.1lf, mass = %10.4lf\n",t,Mass[0]);
+    //loopSize++;
+
+    if (Mass[0] <= criticalMass1) {
+      crossingTemperature = t_old + (t - t_old) * (M_old[0]-criticalMass1)/(M_old[0]-Mass[0]);
+    }
+		
+    t_old=t;
+    M_old[0]=Mass[0];
+  }
+
+  qoiValues[0] = crossingTemperature/beta1;
+	
+  //printf("loopSize = %d\n",loopSize);
+  if ((paramValues.env().verbosity() >= 0) && (paramValues.env().rank() == 0)) {
+    printf("In stage0_qoiRoutine(), A = %g, E = %g, beta1 = %.3lf, criticalMass1 = %.3lf: qoi = %lf.\n",A,E,beta1,criticalMass1,qoiValues[0]);
+  }
+
+  gsl_odeiv_evolve_free (e);
+  gsl_odeiv_control_free(c);
+  gsl_odeiv_step_free   (s);
 
   return;
 }
 
 //********************************************************
-// The MCMC driving routine "uqAppl()": called by main()
+// The validation problem driving routine "uqAppl()": called by main()
 //********************************************************
 template<class P_V,class P_M,class S_V,class S_M,class L_V,class L_M,class Q_V,class Q_M>
 void 
@@ -168,28 +221,20 @@ uqAppl(const uqEnvironmentClass& env)
   uqValidProblemClass<P_V,P_M,L_V,L_M,Q_V,Q_M> validProblem(env);
 
   //******************************************************
-  // Step 2 of 4: Define first validation problem stage
+  // Step 2 of 4: Set the first validation problem stage
   // There are 6 substeps: 2.1, 2.2, 2.3, 2.4, 2.5 and 2.6
   //******************************************************
 
-  //******************************************************
-  // Substep 2.1: Define the prior probability density function object: -2*ln[prior]
-  //******************************************************
-
-  // Use the default prior density, provided by the MCMC library
-  uqProbDensity_BaseClass<P_V,P_M>* stage0_priorParamDensityObj = NULL;
-
-  //******************************************************
-  // Substep 2.2: Define the likelihood function object: -2*ln[likelihood]
-  //******************************************************
-
+  //*****************************************************
+  // Substep 2.0: Code specific to this TGA example
+  //*****************************************************
   // Open input file on experimental data
   FILE *inp;
   inp = fopen("global.dat","r");
 
   // Read kinetic parameters and convert heating rate to K/s
-  double tmpA, tmpE, beta1, variance1;
-  fscanf(inp,"%lf %lf %lf %lf",&tmpA,&tmpE,&beta1,&variance1);
+  double tmpA, tmpE, beta1, variance1, criticalMass1;
+  fscanf(inp,"%lf %lf %lf %lf %lf",&tmpA,&tmpE,&beta1,&variance1,&criticalMass1);
   beta1 /= 60.;
   
   // Read experimental data
@@ -216,6 +261,17 @@ uqAppl(const uqEnvironmentClass& env)
   // Close input file on experimental data
   fclose(inp);
 
+  //******************************************************
+  // Substep 2.1: Define the prior probability density function object: -2*ln[prior]
+  //******************************************************
+
+  // Use the default prior density, provided by the UQ library
+  uqProbDensity_BaseClass<P_V,P_M>* stage0_priorParamDensityObj = NULL;
+
+  //******************************************************
+  // Substep 2.2: Define the likelihood function object: -2*ln[likelihood]
+  //******************************************************
+
   stage0_likelihoodRoutine_DataType<P_V,P_M> stage0_likelihoodRoutine_Data;
   stage0_likelihoodRoutine_Data.beta1     = beta1;
   stage0_likelihoodRoutine_Data.variance1 = variance1;
@@ -229,12 +285,12 @@ uqAppl(const uqEnvironmentClass& env)
   // Substep 2.3: Define the proposal density and proposal generator
   //******************************************************
 
-  // Use the default gaussian proposal and default gaussian generator with the default covariance matrix, all provided by the MCMC library
+  // Use the default gaussian proposal and default gaussian generator with the default covariance matrix, all provided by the UQ library
   P_M*                                    stage0_proposalCovMatrix    = NULL;
   uqProposalDensity_BaseClass<P_V,P_M>*   stage0_proposalDensityObj   = NULL;
   uqProposalGenerator_BaseClass<P_V,P_M>* stage0_proposalGeneratorObj = NULL;
 
-  //P_M* stage0_proposalCovMatrix = stage0_ParamSpace.newMatrix();
+  //P_M* stage0_proposalCovMatrix = validProblem.stage(0).paramSpace().newMatrix();
   //(*stage0_proposalCovMatrix)(0,0) = 1.65122e+10;
   //(*stage0_proposalCovMatrix)(0,1) = 0.;
   //(*stage0_proposalCovMatrix)(1,0) = 0.;
@@ -244,6 +300,8 @@ uqAppl(const uqEnvironmentClass& env)
   // Substep 2.4: Define the propagation input parameters (density and generator)
   //******************************************************
 
+  // Let the 'validProblem' class internally use the parameter generator obtained with the calibration.
+  // The 'uqTgaEx2.inp' file should set 'stage0_propag_inputStageId = 0'.
   uqProbDensity_BaseClass<P_V,P_M>*     stage0_propagParamDensityObj   = NULL;
   uqSampleGenerator_BaseClass<P_V,P_M>* stage0_propagParamGeneratorObj = NULL;
 
@@ -252,12 +310,13 @@ uqAppl(const uqEnvironmentClass& env)
   //******************************************************
 
   stage0_qoiRoutine_DataType<P_V,P_M> stage0_qoiRoutine_Data;
-  stage0_qoiRoutine_Data.beta1 = beta1;
+  stage0_qoiRoutine_Data.beta1         = beta1;
+  stage0_qoiRoutine_Data.criticalMass1 = criticalMass1;
   uqQoIFunction_BaseClass<P_V,P_M,Q_V,Q_M> stage0_qoiFunctionObj(stage0_qoiRoutine<P_V,P_M,S_V,S_M,Q_V,Q_M>,
                                                                  (void *) &stage0_qoiRoutine_Data);
 
   //******************************************************
-  // Substep 2.6: Set the first validation problem stage
+  // Substep 2.6: Instantiate the first validation problem stage
   //******************************************************
 
   validProblem.instantiateStage(0,                              // stage id
@@ -271,13 +330,13 @@ uqAppl(const uqEnvironmentClass& env)
                                 &stage0_qoiFunctionObj);        // step 2.5: propagation qoi function
 
   //******************************************************
-  // Step 3 of 4: Define second validation problem stage
+  // Step 3 of 4: Set the second validation problem stage
   //******************************************************
 
-  // This code example deals with only one stage
+  // This TGA example deals with only one stage
 
   //******************************************************
-  // Step 4 of 4: Quantify the uncertainty
+  // Step 4 of 4: solve the validation problem
   //******************************************************
   validProblem.solve();
 
