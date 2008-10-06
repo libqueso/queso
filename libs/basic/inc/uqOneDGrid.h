2233391bf29e4f144c9de60546398791a9eef88a
Ernesto Prudencio <prudenci@ices.utexas.edu>
2008-10-06 18:22:54 +0000

    forgot one file

---
+++ libs/basic/inc/uqOneDGrid.h
@@ -0,0 +1,178 @@
+/* uq/libs/mcmc/inc/uqOneDGrid.h
+ *
+ * Copyright (C) 2008 The PECOS Team, http://www.ices.utexas.edu/centers/pecos
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __UQ_ONE_D_GRID_FUNCTION_H__
+#define __UQ_ONE_D_GRID_FUNCTION_H__
+
+#include <uqEnvironment.h>
+#include <math.h>
+
+//*****************************************************
+// Classes to accomodate a one dimensional grid
+//*****************************************************
+
+//*****************************************************
+// Base class
+//*****************************************************
+template<class T>
+class uqBaseOneDGridClass {
+public:
+           uqBaseOneDGridClass(const uqEnvironmentClass& env,
+                                 const char* prefix);
+  virtual ~uqBaseOneDGridClass();
+
+  virtual unsigned int size      ()                  const = 0;
+  virtual T            operator[](unsigned int i)    const = 0;
+          void         print     (std::ostream& ofs) const;
+
+protected:
+  const uqEnvironmentClass& m_env;
+        std::string         m_prefix;
+};
+
+template<class T>
+uqBaseOneDGridClass<T>::uqBaseOneDGridClass(
+  const uqEnvironmentClass& env,
+  const char*               prefix)
+  :
+  m_env   (env),
+  m_prefix((std::string)(prefix)+"")
+{
+  if ((m_env.verbosity() >= 5) && (m_env.rank() == 0)) {
+    std::cout << "Entering uqBaseOneDGridClass<T>::constructor()"
+              << ": prefix = " << m_prefix
+              << std::endl;
+  }
+
+  if ((m_env.verbosity() >= 5) && (m_env.rank() == 0)) {
+    std::cout << "Leaving uqBaseOneDGridClass<T>::constructor()"
+              << ": prefix = " << m_prefix
+              << std::endl;
+  }
+}
+
+template<class T>
+uqBaseOneDGridClass<T>::~uqBaseOneDGridClass()
+{
+}
+
+template <class T>
+void
+uqBaseOneDGridClass<T>::print(std::ostream& os) const
+{
+  // Print values *of* grid points
+  os << m_prefix << " = zeros(" << this->size()
+     << ","                          << 1
+     << ");"
+     << std::endl;
+  os << m_prefix << " = [";
+  for (unsigned int j = 0; j < this->size(); ++j) {
+    os << (*this)[j] << " ";
+  }
+  os << "];"
+     << std::endl;
+
+  return;
+}
+
+template <class T>
+std::ostream& operator<< (std::ostream& os, const uqBaseOneDGridClass<T>& obj)
+{
+  obj.print(os);
+  return os;
+}
+
+//*****************************************************
+// Uniform grid class
+//*****************************************************
+template<class T>
+class uqUniformOneDGridClass : public uqBaseOneDGridClass<T> {
+public:
+  uqUniformOneDGridClass(const uqEnvironmentClass& env,
+                         const char*               prefix,
+                               unsigned int        size,
+                               T                   minPosition,
+                               T                   maxPosition);
+ ~uqUniformOneDGridClass();
+
+  unsigned int size      ()               const;
+  T            operator[](unsigned int i) const;
+
+protected:
+  using uqBaseOneDGridClass<T>::m_env;
+  using uqBaseOneDGridClass<T>::m_prefix;
+
+  unsigned int m_size;
+  T            m_minPosition;
+  T            m_maxPosition;
+};
+
+template<class T>
+uqUniformOneDGridClass<T>::uqUniformOneDGridClass(
+  const uqEnvironmentClass& env,
+  const char*               prefix,
+        unsigned int        size,
+        T                   minPosition,
+        T                   maxPosition)
+  :
+  uqBaseOneDGridClass<T>(env,((std::string)(prefix)+"ugrid").c_str()),
+  m_size       (size),
+  m_minPosition(minPosition),
+  m_maxPosition(maxPosition)
+{
+  if ((m_env.verbosity() >= 5) && (m_env.rank() == 0)) {
+    std::cout << "Entering uqUniformOneDGridClass<T>::constructor()"
+              << ": prefix = " << m_prefix
+              << std::endl;
+  }
+
+  if ((m_env.verbosity() >= 5) && (m_env.rank() == 0)) {
+    std::cout << "Leaving uqUniformOneDGridClass<T>::constructor()"
+              << ": prefix = " << m_prefix
+              << std::endl;
+  }
+}
+
+template<class T>
+uqUniformOneDGridClass<T>::~uqUniformOneDGridClass()
+{
+}
+
+template<class T>
+unsigned int
+uqUniformOneDGridClass<T>::size() const
+{
+  return m_size;
+}
+
+template<class T>
+T
+uqUniformOneDGridClass<T>::operator[](unsigned int i) const
+{
+  UQ_FATAL_TEST_MACRO(i >= m_size,
+                      m_env.rank(),
+                      "uqUniformOneDGridClass<V,M>::operator[]",
+                      "too large i");
+
+  T ratio = ((T) i)/(((T)m_size)-1.); // IMPORTANT: Yes, '-1.'
+  T position = (1.-ratio)*m_minPosition + ratio*m_maxPosition;
+  return position;
+}
+
+#endif // __UQ_ONE_D_GRID_FUNCTION_H__
