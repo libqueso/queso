e6a82a0d8740189761f8966bb65e7038066221c7
Ernesto Prudencio <prudenci@ices.utexas.edu>
2009-03-06 16:27:01 +0000

    QUESO 0.4.0: example 'statisticalInverseProblem1' working

--- examples/statisticalInverseProblem1/tests/test_2009_02_03/sip_plot.m
+++ examples/statisticalInverseProblem1/tests/test_2009_02_03/sip_plot.m
@@ -1,10 +1,12 @@
-uqNormalOutput
-uqNormalExtraOutput
+cd outputData
+sipOutput_sub0
+sipExtraOutput_sub0
+cd ..
 load nada
 
 d=nada(:,2);    
 [nsimu,temp]=size(d);
-[temp,npar]=size(ip_mc_chain);
+[temp,npar]=size(ip_mc_chain_sub0);
 meanVec=[0 0 0 0];
 covMatrix = [0.263584 0.0820654 0.0748222 -0.410472 
 0.0820654 0.0414596 0.0241198 -0.137645 
@@ -18,7 +20,7 @@ c95  = chiqf(0.95,npar);
 cc50 = sum(d<c50)./nsimu;
 cc95 = sum(d<c95)./nsimu;
 
-plot(ip_mc_chain(:,1),ip_mc_chain(:,2),'.');
+plot(ip_mc_chain_sub0(:,1),ip_mc_chain_sub0(:,2),'.');
 xlabel(ip_mc_componentNames(1));
 ylabel(ip_mc_componentNames(2));
 title(sprintf('Rejected = %.1f%%, c50 = %.1f%%, c95 = %.1f%%', ...
@@ -32,11 +34,11 @@ ellipse(meanVec(1:2),c95*covMatrix(1:2,1:2),'r--','LineWidth',2);
 waitforbuttonpress;
 clf;
 
-plot(ip_mc_chain_kdeEvalPositions(1,:),ip_mc_chain_gaussianKdeDensities(1,:),'-b');
+plot(ip_mc_chain_GkdePosits_sub0(1,:),ip_mc_chain_GkdeValues_sub0(1,:),'-b');
 hold
-plot(ip_mc_chain_kdeEvalPositions(2,:),ip_mc_chain_gaussianKdeDensities(2,:),'-r');
-plot(ip_mc_chain_kdeEvalPositions(3,:),ip_mc_chain_gaussianKdeDensities(3,:),'--b');
-plot(ip_mc_chain_kdeEvalPositions(4,:),ip_mc_chain_gaussianKdeDensities(4,:),'--r');
+plot(ip_mc_chain_GkdePosits_sub0(2,:),ip_mc_chain_GkdeValues_sub0(2,:),'-r');
+plot(ip_mc_chain_GkdePosits_sub0(3,:),ip_mc_chain_GkdeValues_sub0(3,:),'--b');
+plot(ip_mc_chain_GkdePosits_sub0(4,:),ip_mc_chain_GkdeValues_sub0(4,:),'--r');
 
 ylabel('marginal pdf','fontsize',20);
 xlabel('Parameter values','fontsize',20);
