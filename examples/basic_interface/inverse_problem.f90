adc8e34875b39ada0d585ac7b54673a34025b8a2
Karl Schulz <karl@ices.utexas.edu>
2009-07-03 03:57:06 +0000

    queso [branch]: proper headers for toy examples

--- examples/basic_interface/inverse_problem.f90
+++ examples/basic_interface/inverse_problem.f90
@@ -1,8 +1,35 @@
-
-!----------------------------------------
-! A simple Calibration example using the
-! basic QUESO interface..
-!---------------------------------------
+!--------------------------------------------------------------------------
+!--------------------------------------------------------------------------
+!
+! Copyright (C) 2008,2009 The PECOS Development Team
+!
+! Please see http://pecos.ices.utexas.edu for more information.
+!
+! This file is part of the QUESO Library (Quantification of Uncertainty
+! for Estimation, Simulation and Optimization).
+!
+! QUESO is free software: you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! QUESO is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+! GNU General Public License for more details.
+!
+! You should have received a copy of the GNU General Public License
+! along with QUESO. If not, see <http://www.gnu.org/licenses/>.
+!
+!--------------------------------------------------------------------------
+!
+! $Id$
+!
+! Simple toy problem to illustrate the use of the Basic QUESO
+! interface to perform a statistical inverse problem in Fortran..
+! 
+!--------------------------------------------------------------------------
+!-------------------------------------------------------------------------- 
 
 program main
   integer :: ierr
