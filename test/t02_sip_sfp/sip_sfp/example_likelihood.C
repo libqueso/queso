f5daa997c8547b1d093f67ecfeb08f5737e988db
Ernesto Prudencio <prudenci@ices.utexas.edu>
2009-07-10 21:48:36 +0000

    QUESO 0.4.0: adding test 02. Not running yet, though.

---
+++ test/t02_sip_sfp/sip_sfp/example_likelihood.C
@@ -0,0 +1,51 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2008 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of the QUESO Library (Quantification of Uncertainty
+ * for Estimation, Simulation and Optimization).
+ *
+ * QUESO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QUESO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QUESO. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * $Id$
+ *
+ * Brief description of this file: 
+ * 
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#include <example_likelihood.h>
+
+double likelihoodRoutine(
+  const uqGslVectorClass& paramValues,
+  const uqGslVectorClass* paramDirection,
+  const void*             functionDataPtr,
+  uqGslVectorClass*       gradVector,
+  uqGslMatrixClass*       hessianMatrix,
+  uqGslVectorClass*       hessianEffect)
+{
+  const uqGslVectorClass& meanVector =
+    *((likelihoodRoutine_DataType *) functionDataPtr)->meanVector;
+  const uqGslMatrixClass& covMatrix  =
+    *((likelihoodRoutine_DataType *) functionDataPtr)->covMatrix;
+
+  uqGslVectorClass diffVec(paramValues - meanVector);
+
+  return scalarProduct(diffVec, covMatrix.invertMultiply(diffVec));
+}
