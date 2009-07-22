4c73739e8af2db993c4f20c165bbde3e13d40325
Ernesto Prudencio <prudenci@ices.utexas.edu>
2009-07-22 14:25:13 +0000

    QUESO 0.4.0: updated day of 0.4.0 release in uqEnvironment.C, to July 22, 2009

--- libs/basic/src/uqEnvironment.C
+++ libs/basic/src/uqEnvironment.C
@@ -42,10 +42,10 @@
 // Version "0.21"  on "Oct/08/2008"
 // Version "0.3.0" on "Feb/13/2009"
 // Version "0.3.1" on "Feb/19/2009"
-// Version "0.4.0" on "Jul/13/2009"
+// Version "0.4.0" on "Jul/22/2009"
 // Version "0.5.0" on "MMM/DD/2009"
 #define QUESO_LIBRARY_CURRENT_VERSION "0.4.0"
-#define QUESO_LIBRARY_RELEASE_DATE    "Jul/13/2009"
+#define QUESO_LIBRARY_RELEASE_DATE    "Jul/22/2009"
 
 //*****************************************************
 // Base class
