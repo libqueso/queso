a9f118d2ffe17705d737de6b0a0d826ee05fba34
Ernesto Prudencio <prudenci@ices.utexas.edu>
2009-09-08 22:20:31 +0000

    QUESO 0.4.1: added print commands to sfp_plot.m

--- examples/statisticalForwardProblem/tests/test_2009_02_11/sfp_plot.m
+++ examples/statisticalForwardProblem/tests/test_2009_02_11/sfp_plot.m
@@ -5,16 +5,19 @@ sfpOutput_sub0
 
 plot(fp_mc_QoiSeq_corrViaFftLags_sub0,fp_mc_QoiSeq_corrViaFftInitPos0_sub0,'-o');
 title('QoI autocorrelation');
+print -dpng fig1.png
 waitforbuttonpress;
 clf;
 
 plot(fp_mc_QoiSeq_unifGkdePosits_sub0,fp_mc_QoiSeq_unifGkdeValues_sub0,'-');
 title('QoI KDE');
+print -dpng fig2.png
 waitforbuttonpress;
 clf;
 
 plot(fp_unifQoiCdf_0_grid_sub0,fp_unifQoiCdf_0_values_sub0,'-');
 title('QoI cdf');
+print -dpng fig3.png
 waitforbuttonpress;
 clf;
 
