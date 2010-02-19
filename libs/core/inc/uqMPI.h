b87ee18bda815854c6b3d94a630f36429d48336a
Ernesto Prudencio <prudenci@ices.utexas.edu>
2010-02-19 16:14:30 +0000

    forgot to add these files on the previous commit

---
+++ libs/core/inc/uqMPI.h
@@ -0,0 +1,42 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2008 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of the QUESO Library (Quantification of Uncertainty
+ * for Estimation, Simulation and Optimization).
+ *
+ * QUESO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QUESO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QUESO. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * $Id$
+ *
+ * Brief description of this file: 
+ * 
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#ifndef __UQ_MPI_H__
+#define __UQ_MPI_H__
+
+#include <uqDefines.h>
+#include <Epetra_MpiComm.h>
+
+int UQ_MPI_Barrier(MPI_Comm comm);
+int UQ_MPI_Barrier(const Epetra_MpiComm& comm);
+
+#endif // __UQ_MPI_H__
