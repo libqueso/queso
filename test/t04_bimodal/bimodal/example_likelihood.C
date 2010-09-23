0ce571cc1cd73feef8141fe7e2e765aadffa66bf
Ernesto Prudencio <prudenci@ices.utexas.edu>
2010-09-23 01:39:29 +0000

    adding t04_bimodal to 'make check'

---
+++ test/t04_bimodal/bimodal/example_likelihood.C
@@ -0,0 +1,97 @@
+/*--------------------------------------------------------------------------
+ *--------------------------------------------------------------------------
+ *
+ * Copyright (C) 2008 The PECOS Development Team
+ *
+ * Please see http://pecos.ices.utexas.edu for more information.
+ *
+ * This file is part of the QUESO Library (Quantification of Uncertainty
+ * for Estimation, Simulation and Optimization).
+ *
+ * QUESO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QUESO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QUESO. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *--------------------------------------------------------------------------
+ *
+ * $Id$
+ *
+ * Brief description of this file: 
+ * 
+ *--------------------------------------------------------------------------
+ *-------------------------------------------------------------------------- */
+
+#include <example_likelihood.h>
+
+double likelihoodRoutine(
+  const uqGslVectorClass& paramValues,
+  const uqGslVectorClass* paramDirection,
+  const void*             functionDataPtr,
+  uqGslVectorClass*       gradVector,
+  uqGslMatrixClass*       hessianMatrix,
+  uqGslVectorClass*       hessianEffect)
+{
+  //const uqGslVectorClass& meanVector =
+  //  *((likelihoodRoutine_DataType *) functionDataPtr)->meanVector;
+  //const uqGslMatrixClass& covMatrix  =
+  //  *((likelihoodRoutine_DataType *) functionDataPtr)->covMatrix;
+
+  //uqGslVectorClass diffVec(paramValues - meanVector);
+
+  //return scalarProduct(diffVec, covMatrix.invertMultiply(diffVec));
+
+  double x = paramValues[0];
+
+  double mean1  = 10.;
+  double sigma1 = 1.;
+  double y1 = (x-mean1)*(x-mean1)/(2.*sigma1*sigma1);
+  double z1 = (1./sigma1/sqrt(2*M_PI))*exp(-y1);
+
+  double mean2  = 100.;
+  double sigma2 = 5.;
+  double y2 = (x-mean2)*(x-mean2)/(2.*sigma2*sigma2);
+  double z2 = (1./sigma2/sqrt(2*M_PI))*exp(-y2);
+
+  double resultValue = -2*log((z1+2.*z2)/3.);
+
+  if (resultValue == INFINITY) {
+    //std::cerr << "WARNING In likelihoodRoutine"
+    //          << ", fullRank "       << paramValues.env().fullRank()
+    //          << ", subEnvironment " << paramValues.env().subId()
+    //          << ", subRank "        << paramValues.env().subRank()
+    //          << ", inter0Rank "     << paramValues.env().inter0Rank()
+    //          << ": x = "            << x
+    //          << ", z1 = "           << z1
+    //          << ", z2 = "           << z2
+    //          << ", resultValue = "  << resultValue
+    //          << std::endl;
+    resultValue = 1040.;
+  }
+
+#ifdef QUESO_EXPECTS_LN_LIKELIHOOD_INSTEAD_OF_MINUS_2_LN
+  double returnValue = -.5*resultValue;
+#else
+  double returnValue = resultValue; 
+#endif
+
+  if (paramValues.env().exceptionalCircunstance()) {
+    if ((paramValues.env().subDisplayFile()       ) &&
+        (paramValues.env().displayVerbosity() >= 0)) { // detailed output debug
+      *paramValues.env().subDisplayFile() << "Leaving likelihood function"
+                                          << ": paramValues = " << paramValues
+                                          << ", returnValue = " << returnValue
+                                          << std::endl;
+    }
+  }
+
+  return returnValue;
+}
