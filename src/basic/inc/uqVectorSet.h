be201fea0a10af1f7c17ab98a59d5df829b0632d
Karl Schulz <karl@ices.utexas.edu>
2010-10-03 17:05:00 +0000

    [queso]: including license header template (Issue #929)

--- src/basic/inc/uqVectorSet.h
+++ src/basic/inc/uqVectorSet.h
@@ -1,34 +1,9 @@
-/*--------------------------------------------------------------------------
- *--------------------------------------------------------------------------
- *
- * Copyright (C) 2008 The PECOS Development Team
- *
- * Please see http://pecos.ices.utexas.edu for more information.
- *
- * This file is part of the QUESO Library (Quantification of Uncertainty
- * for Estimation, Simulation and Optimization).
- *
- * QUESO is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * QUESO is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with QUESO. If not, see <http://www.gnu.org/licenses/>.
- *
- *--------------------------------------------------------------------------
- *
- * $Id$
- *
- * Brief description of this file: 
- * 
- *--------------------------------------------------------------------------
- *-------------------------------------------------------------------------- */
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
 
 #ifndef __UQ_VECTOR_SET_H__
 #define __UQ_VECTOR_SET_H__
