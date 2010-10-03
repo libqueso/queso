2b2d21f79049e93ded96a3a4fcfe602ac7152a53
Karl Schulz <karl@ices.utexas.edu>
2010-10-03 17:58:49 +0000

    [queso]: fix extra header

--- test/t02_sip_sfp/sip_sfp/example_main.C
+++ test/t02_sip_sfp/sip_sfp/example_main.C
@@ -25,25 +25,6 @@
 // $Id$
 //
 //--------------------------------------------------------------------------
- * routine 'compute(*env)'. This routine is called right after the initialization
- * of the MPI environment and of the QUESO environment and is available in
- * file 'example_compute.C'.
- *
- * It is easier to understand this example after undertanding two other simpler
- * examples:
- * (1) examples/statisticalInverseProblem1/src/exStatisticalInverseProblem1_gsl.C
- * (2) examples/statisticalForwardProblem/src/exStatisticalForwardProblem_gsl.C
- *
- * Example (1) focuses on the templated class uqStatisticalInverseProblemClass<P_V,P_M>.
- * Example (2) focuses on the templated class uqStatisticalForwardProblemClass<P_V,P_M,Q_V,Q_M>.
- * 
- * This example uses both statistical problem templated classes, once each,
- * since it uses the solution of the inverse problem as input to the forward problem.
- * 
- * This example is explained in detail in the user's manual (pdf file)
- *
- *--------------------------------------------------------------------------
- *-------------------------------------------------------------------------- */
 
 #include <example_compute.h>
 
