c1fd082c613809df5c99e070935a12cebde7e7e7
Karl Schulz <karl@ices.utexas.edu>
2010-10-03 18:16:39 +0000

    [queso]: use updated name for header

--- examples/basic_interface/inverse_problem.c
+++ examples/basic_interface/inverse_problem.c
@@ -34,7 +34,7 @@
 #include <inverse_problem.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <queso.h>
+#include <queso_basic.h>
 #include <mpi.h> // Added by prudenci on 2009/Sep/06 in order to avoid INTEL warning. Maybe Karl want to add that in queso.h (a grvy file?)
 
 int main(int argc, char *argv[])
