34d72799fa94c3e5733cb2fb01fad8c0640f3af1
Karl Schulz <karl@ices.utexas.edu>
2012-05-24 14:12:40 +0000

    [queso]: updating F90 interface example for James Strand; working with new openMPI that is now on hera

--- examples/statisticalInverse_Fortran/src/build_me_hera.sh
+++ examples/statisticalInverse_Fortran/src/build_me_hera.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+# compile fortran likelihood function
 mpif90 -c -g likelihood_function.f90
 
-mpic++ -g -I./ \
-    -I$QUESO_DIR/include \
-    -I$TRILINOS_DIR/include \
-    -I$BOOST_DIR/include \
-    -I$HDF5_DIR/include  \
-    -I$GLPK_DIR/include  \
-    -DMPICH_IGNORE_CXX_SEEK \
-    exStatisticalInverseProblem1_gsl.C likelihood_function.o -L$QUESO_DIR/lib -Wl,-rpath,$QUESO_DIR/lib -lqueso
+
+# compile queso main which calls fortran likelihood
+
+mpic++ -c -g -I./ -I$QUESO_DIR/include -I$BOOST_DIR/include \
+    -I$HDF5_DIR/include -DMPICH_IGNORE_CXX_SEEK exStatisticalInverseProblem1_gsl.C 
+
+# link final executable
+
+mpif90 -nofor-main -o queso_app likelihood_function.o exStatisticalInverseProblem1_gsl.o \
+    -L$QUESO_DIR/lib -Wl,-rpath,$QUESO_DIR/lib -lqueso
+
+#mpic++ -g -I./ \
+#    -I$QUESO_DIR/include \
+#    -I$BOOST_DIR/include \
+#    -I$HDF5_DIR/include  \
+#    -DMPICH_IGNORE_CXX_SEEK \
+#    exStatisticalInverseProblem1_gsl.C likelihood_function.o -L$QUESO_DIR/lib -Wl,-rpath,$QUESO_DIR/lib -lqueso -lifcore
+
+
+#    -I$TRILINOS_DIR/include \
+#    -I$GLPK_DIR/include  \
