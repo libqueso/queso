e85db4f2122aa540af13facc0d5369cddb150260
Kemelli Estacio-Hiroms <kemelli@ices.utexas.edu>
2012-11-28 20:15:05 +0000

    queso/trunk/examples/validationCycle and validationCycle2: renaming output figs in tga_cycle_plot.m.

--- examples/validationCycle/tests/test_2012_11_15/tga_cycle_plot.m
+++ examples/validationCycle/tests/test_2012_11_15/tga_cycle_plot.m
@@ -1,15 +1,12 @@
 cd outputData
-%sfpOutput_sub0
-%sfpExtraOutput_sub0
-%cd ..
+
 file_val_fp_qoi2;
 file_cal_fp_qoi2;
 file_cal_ip_raw;
 file_val_ip_raw;
 
-%Prior
-%-------------------------------------------------------------------------
-
+% Priors ------------------------------------------------------------------
+% Parameter A 
 minPrior0 = 2.40e+11;
 maxPrior0 = 2.80e+11;
 numPrior0 = 200;
@@ -30,17 +27,17 @@ cal_right_vertical_line_x0 = ones(1,11)*maxPrior0;
 cal_right_vertical_line_y0 = [0 : cycle_cal_ip_prior_0_values(1,numHorizPts0+1)/10 : cycle_cal_ip_prior_0_values(1,numHorizPts0+1)];
 plot(cal_right_vertical_line_x0,cal_right_vertical_line_y0,'b--','linewidth',1);
 
-ylabel('Prior marginal pdf','fontsize',20);
+ylabel('Prior marginal PFD','fontsize',20);
 xlabel('A (min^{-1})','fontsize',20);
-title('Parameter A: prior marginal pdf','fontsize',20);
+title('Parameter A: prior marginal PFD','fontsize',20);
 grid on;
 set(gca,'fontsize',20);
-print -dpng cal_prior_0_sub0.png
+print -dpng cal_parameter1_prior.png
 waitforbuttonpress;
 clf
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Parameter E 
 minPrior1 = 1.80e+5;
 maxPrior1 = 2.20e+5;
 numPrior1 = 200;
@@ -61,19 +58,19 @@ cal_right_vertical_line_x1 = ones(1,11)*maxPrior1;
 cal_right_vertical_line_y1 = [0 : cycle_cal_ip_prior_1_values(1,numHorizPts1+1)/10 : cycle_cal_ip_prior_1_values(1,numHorizPts1+1)];
 plot(cal_right_vertical_line_x1,cal_right_vertical_line_y1,'b--','linewidth',1);
 
-ylabel('Prior marginal pdf','fontsize',20);
+ylabel('Prior marginal PFD','fontsize',20);
 xlabel('E (J/mol)','fontsize',20);
-title('Parameter E: prior marginal pdf','fontsize',20);
+title('Parameter E: prior marginal PFD','fontsize',20);
 grid on;
 set(gca,'fontsize',20);
-print -dpng unif_cal_prior_1.png
+print -dpng cal_parameter2_prior.png
 waitforbuttonpress;
 clf
 
 
 
 
-%FORWARD
+% FORWARD
 %-------------------------------------------------------------------------
 % Autocorrelation
 nlags=10;
@@ -85,25 +82,25 @@ set(gca,'fontsize',20);
 title('QoI autocorrelation');
 legend('calibration',...
        'validation',...
-       'location','northwest');
-print -dpng fig1.png
+       'location','northeast');
+print -dpng cal_val_QoI_autocorrelation.png
 waitforbuttonpress;
 clf;
 
-%KDE
+% PDF
 [f,xi] = ksdensity(cycle_cal_fp_mc_QoiSeq_unified,'function','pdf');
 [fv,xiv] = ksdensity(cycle_val_fp_mc_QoiSeq_unified,'function','pdf');
 plot(xi,f,'r-',xiv,fv,'b-','linewidth',3);
 grid minor;
 axis([0 1.0 0 5]);
 set(gca,'fontsize',20);
-ylabel('Pdf','fontsize',20);
+ylabel('pdf','fontsize',20);
 xlabel('Mass fraction remaining at t=3.9s','fontsize',20);
-title('QoI Pdf','fontsize',20);
+title('QoI PFD','fontsize',20);
 legend('calibration',...
        'validation',...
-       'location','northwest');
-print -dpng fig2.png
+       'location','northeast');
+print -dpng cal_val_QoI_PDF.png
 waitforbuttonpress;
 clf;
 
@@ -121,17 +118,16 @@ axis([0 1.0 a(3) a(4)]);
 grid minor;
 set(gca,'fontsize',20);
 title('QoI CDF');
-ylabel('Cdf','fontsize',20);
+ylabel('CFD','fontsize',20);
 xlabel('Mass fraction remaining at t=3.9s','fontsize',20);
 legend('calibration',...
        'validation',...
-       'location','northwest');
-print -dpng fig3.png
+       'location','southeast');
+print -dpng cal_val_QoI_CDF.png
 waitforbuttonpress;
 clf;
 
 %CDF full picture
-
 plot(xi,f,'r-',xiv,fv,'b-','linewidth',3);
 hold
 epsilon = 0.06;
@@ -140,15 +136,15 @@ plot([0:0.01:1],ones(101,1)*(1.-epsilon/2),'k--','linewidth',1);
 
 a=axis;
 axis([0 1.0 a(3) a(4)]);
-ylabel('Cdf','fontsize',20);
+ylabel('CFD','fontsize',20);
 xlabel('Mass fraction remaining at t=3.9s','fontsize',20);
 title('Full Picture','fontsize',20);
-grid on;
+grid minor;
 set(gca,'fontsize',20);
 legend('Calibration',...
        'Validation',...
-       'location','northwest');
-print -dpng cal_val_qoi_Cdf_0_model_confidence_a_sub0.png
+       'location','best');
+print -dpng cal_val_QoI_CDF_model_confidence_full.png
 waitforbuttonpress;
 clf
 
@@ -158,7 +154,7 @@ clf
 [fv,xiv] = ksdensity(cycle_val_fp_mc_QoiSeq_unified,'function','cdf');
 plot(xi,f,'r-',xiv,fv,'b-','linewidth',3);
 axis([0.15 0.27 .02 .15]);
-ylabel('Cdf','fontsize',20);
+ylabel('CFD','fontsize',20);
 xlabel('Mass fraction remaining at t=3.9s','fontsize',20);
 title('Zoom','fontsize',20);
 grid minor;
@@ -166,25 +162,25 @@ set(gca,'fontsize',20);
 legend('Calibration',...
        'Validation',...
        'location','northwest');
-print -dpng cal_val_qoi_Cdf_0_model_confidence_b_sub0.png
+print -dpng cal_val_QoI_CDF_model_confidence_zoom.png
 waitforbuttonpress;
 clf
 
 
 %INVERSE
 %------------------------------------------------------------------------
-% Parameter 1 ------------
+% Parameter 1 - A ------------
 % KDE plots 
 [f1,x1] = ksdensity(cycle_cal_ip_mh_rawChain_unified(1,:),'function','pdf');
 [f2,x2] = ksdensity(cycle_val_ip_mh_rawChain_unified(1,:),'function','pdf');
 plot(x1,f1,'-b',x2,f2,'-r','linewidth',3)
-title('Posterior marginal pdfs of parameter 1','fontsize',20);
+title('Posterior marginal PDFs of parameter A','fontsize',20);
 grid minor;
 set(gca,'fontsize',20);
 legend('calibration',...
        'validation',...
        'location','northwest');
-print -dpng fig4.png
+print -dpng cal_val_parameter1_PDF.png
 waitforbuttonpress;
 clf;
 
@@ -193,27 +189,28 @@ clf;
 [f1,x1] = ksdensity(cycle_cal_ip_mh_rawChain_unified(1,:),'function','cdf');
 [f2,x2] = ksdensity(cycle_val_ip_mh_rawChain_unified(1,:),'function','cdf');
 plot(x1,f1,'-b',x2,f2,'-r','linewidth',3)
-title('Cdfs of parameter 1','fontsize',20);
+title('CFDs of parameter 1','fontsize',20);
 grid minor;
 set(gca,'fontsize',20);
 legend('calibration',...
        'validation',...
        'location','northwest');
-print -dpng fig5.png
+print -dpng cal_val_parameter1_CDF.png
 waitforbuttonpress;
 clf;
-% Parameter 2 ------------
+
+% Parameter 2 - E ------------
 % KDE plots 
 [f1,x1] = ksdensity(cycle_cal_ip_mh_rawChain_unified(2,:),'function','pdf');
 [f2,x2] = ksdensity(cycle_val_ip_mh_rawChain_unified(2,:),'function','pdf');
 plot(x1,f1,'-b',x2,f2,'-r','linewidth',3)
-title('Posterior marginal pdfs of parameter 2','fontsize',20);
+title('Posterior marginal PFDs of parameter E','fontsize',20);
 grid minor;
 set(gca,'fontsize',20);
 legend('calibration',...
        'validation',...
        'location','northwest');
-print -dpng fig6.png
+print -dpng cal_val_parameter2_PDF.png
 waitforbuttonpress;
 clf;
 
@@ -222,13 +219,13 @@ clf;
 [f1,x1] = ksdensity(cycle_cal_ip_mh_rawChain_unified(2,:),'function','cdf');
 [f2,x2] = ksdensity(cycle_val_ip_mh_rawChain_unified(2,:),'function','cdf');
 plot(x1,f1,'-b',x2,f2,'-r','linewidth',3)
-title('Cdfs of parameter 2','fontsize',20);
+title('CFDs of parameter E','fontsize',20);
 grid minor;
 set(gca,'fontsize',20);
 legend('calibration',...
        'validation',...
        'location','northwest');
-print -dpng fig7.png
+print -dpng cal_val_parameter2_CDF.png
 waitforbuttonpress;
 clf;
 
