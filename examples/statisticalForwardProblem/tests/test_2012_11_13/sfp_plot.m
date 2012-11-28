fea390cb6c75addd3f2d6c81473d2520fa430259
Kemelli Estacio-Hiroms <kemelli@ices.utexas.edu>
2012-11-28 19:10:25 +0000

    queso/trunk/examples/statisticalForwardProblem/tests/test_2012_11_13: renaming output figs.

--- examples/statisticalForwardProblem/tests/test_2012_11_13/sfp_plot.m
+++ examples/statisticalForwardProblem/tests/test_2012_11_13/sfp_plot.m
@@ -1,28 +1,34 @@
 cd outputData
-%sfpOutput_sub0
-%sfpExtraOutput_sub0
-%cd ..
 file_sfp_qoi
 
+% Plotting QoI autocorrelation -------------------------------------------
 nlags=10;
 [ACF, lags, bounds] = autocorr(fp_mc_QoiSeq_unified, nlags, 0);
 plot(lags,ACF,'mo-','linewidth',3);
+grid minor;
+set(gca,'fontsize',20);
 title('QoI autocorrelation');
-print -dpng fig1.png
+print -dpng QoI_autocorrelation.png
 waitforbuttonpress;
 clf;
 
+% Plotting QoI marginal posterior pdf ------------------------------------
 [f,xi] = ksdensity(fp_mc_QoiSeq_unified,'function','pdf');
-plot(xi,f,'-m','linewidth',3)
+plot(xi,f,'-m','linewidth',3);
+set(gca,'fontsize',20);
+grid minor;
 title('QoI KDE');
-print -dpng fig2.png
+print -dpng QoI_PDF.png
 waitforbuttonpress;
 clf;
 
+% Plotting QoI marginal posterior cdf ------------------------------------
 [f,xi] = ksdensity(fp_mc_QoiSeq_unified,'function','cdf');
-plot(xi,f,'-m','linewidth',3)
+plot(xi,f,'-m','linewidth',3);
+set(gca,'fontsize',20);
+grid minor;
 title('QoI cdf');
-print -dpng fig3.png
+print -dpng QoI_CDF.png
 waitforbuttonpress;
 clf;
 
