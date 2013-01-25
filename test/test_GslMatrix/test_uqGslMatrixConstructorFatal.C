439b0449026e592a42e2e0fc692211481a634935
Damon McDougall <damon@ices.utexas.edu>
2013-01-25 05:28:25 +0000

    [queso]: Add first gsl matrix test

---
+++ test/test_GslMatrix/test_uqGslMatrixConstructorFatal.C
@@ -0,0 +1,25 @@
+#include <uqGslMatrix.h>
+
+#ifdef QUESO_HAS_MPI
+#include <mpi.h>
+#endif
+
+using namespace std;
+
+int main(int argc, char **argv) {
+#ifdef QUESO_HAS_MPI
+  MPI_Init(&argc, &argv);
+#endif
+
+  uqGslMatrixClass matrix;
+
+#ifdef QUESO_HAS_MPI
+  MPI_Finalize();
+#endif
+
+  /*
+   * This code should never get here. If it does, the bash script that wraps
+   * around it negates the return value, making this a failure
+   */
+  return 0;
+}
