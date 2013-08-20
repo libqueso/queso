22f5706157df2f06c15d762cd0d1d78a60c16261
Ernesto Prudencio <prudenci@ices.utexas.edu>
2013-08-20 00:43:47 +0000

    adding verification problem 3. Will work on it in the next hours.

---
+++ examples/brian_williams_verif3/inc/verif3_gsl.h
@@ -0,0 +1,21 @@
+#ifndef __VERIF3_GSL_H__
+#define __VERIF3_GSL_H__
+
+#include <uqGslMatrix.h>
+
+void   solveSip         (const uqFullEnvironmentClass& env);
+
+double likelihoodRoutine(const uqGslVectorClass& paramValues,
+                         const uqGslVectorClass* paramDirection,
+                         const void*             functionDataPtr,
+                         uqGslVectorClass*       gradVector,
+                         uqGslMatrixClass*       hessianMatrix,
+                         uqGslVectorClass*       hessianEffect);
+
+struct likelihoodDataStruct {
+  uqGslVectorClass* aVec;    // p x 1
+  double            sigmaEps;
+  uqGslVectorClass* ySamples; // n x 1
+};
+
+#endif // __VERIF3_GSL_H__
