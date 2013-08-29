7dc3c9d87945ed426d0b2b246168284d22b3d269
Ernesto Prudencio <prudenci@ices.utexas.edu>
2013-08-29 00:29:57 +0000

    backup

---
+++ examples/brian_williams_verif6/inc/verif6_gsl.h
@@ -0,0 +1,22 @@
+#ifndef __VERIF6_GSL_H__
+#define __VERIF6_GSL_H__
+
+#include <uqGslMatrix.h>
+
+void   solveSip         (const uqFullEnvironmentClass& env, bool useML);
+
+double likelihoodRoutine(const uqGslVectorClass& paramValues,
+                         const uqGslVectorClass* paramDirection,
+                         const void*             functionDataPtr,
+                         uqGslVectorClass*       gradVector,
+                         uqGslMatrixClass*       hessianMatrix,
+                         uqGslVectorClass*       hessianEffect);
+
+struct likelihoodDataStruct {
+  uqGslVectorClass* aVec;    // p x 1
+  uqGslVectorClass* bVec;    // p x 1
+  double            sigmaTotal;
+  uqGslVectorClass* ySamples; // n x 1
+};
+
+#endif // __VERIF6_GSL_H__
