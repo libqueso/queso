5c7a84a2c4f74f468ac1eaaec6efe8f61e86662d
Damon McDougall <damon@ices.utexas.edu>
2013-09-12 23:19:19 -0500

    Add DistArray ctor test

---
+++ test/test_DistArray/test_DistArrayCtor.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <queso/DistArray.h>
+
+int main(int argc, char **argv) {
+  QUESO::DistArray<std::string> d();
+
+  /*
+   * This code should never get here. If it does, the bash script that wraps
+   * around it negates the return value, making this a failure
+   */
+  return 0;
+}
