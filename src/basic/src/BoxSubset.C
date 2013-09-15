d21c8e435048d5971b740dd9054e0838aeafdf3f
Damon McDougall <damon@ices.utexas.edu>
2013-09-15 16:49:55 -0500

    Add BoxSubset implementation file

---
+++ src/basic/src/BoxSubset.C
@@ -0,0 +1,107 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#include <queso/VectorSpace.h>
+
+namespace QUESO {
+
+// Default, shaped constructor
+template<class V, class M>
+BoxSubset<V,M>::BoxSubset(const char* prefix,
+    const VectorSpace<V,M>& vectorSpace,
+    const V& minValues,
+    const V& maxValues)
+  : VectorSubset<V,M>(prefix,vectorSpace,0.),
+    m_minValues(minValues),
+    m_maxValues(maxValues)
+{
+  UQ_FATAL_TEST_MACRO(minValues.sizeLocal() != maxValues.sizeLocal(),
+                      m_env.worldRank(),
+                      "BoxSubset<V,M>::BoxSubset()",
+                      "vectors 'minValues' and 'maxValues' should have the same size");
+  UQ_FATAL_TEST_MACRO(minValues.sizeLocal() != vectorSpace.dimLocal(),
+                      m_env.worldRank(),
+                      "BoxSubset<V,M>::BoxSubset()",
+                      "sizes of vectors 'minValues' and 'maxValues' should be equal to dimension of the vector space");
+  for (unsigned int i = 0; i < m_vectorSpace->dimLocal(); ++i) {
+    UQ_FATAL_TEST_MACRO(minValues[i] > maxValues[i],
+                        m_env.worldRank(),
+                        "BoxSubset<V,M>::BoxSubset()",
+                        "it should happen minValue <= maxValue for all dimensions");
+  }
+
+  m_volume = 1.;
+  for (unsigned int i = 0; i < m_vectorSpace->dimLocal(); ++i) {
+    m_volume *= (m_maxValues[i] - m_minValues[i]);
+  }
+}
+
+// Destructor
+template<class V, class M>
+BoxSubset<V,M>::~BoxSubset()
+{
+}
+
+// Math methods
+template<class V, class M>
+bool BoxSubset<V,M>::contains(const V& vec) const
+{
+  // prudenci, 2012-09-26: allow boundary values because of 'beta' realizer, which can generate a sample with boundary value '1'
+  //return (!vec.atLeastOneComponentSmallerOrEqualThan(m_minValues) &&
+  //        !vec.atLeastOneComponentBiggerOrEqualThan (m_maxValues));
+  return (!vec.atLeastOneComponentSmallerThan(m_minValues) &&
+          !vec.atLeastOneComponentBiggerThan (m_maxValues));
+}
+
+
+template<class V, class M>
+const V& BoxSubset<V,M>::minValues() const
+{
+  return m_minValues;
+}
+
+template<class V, class M>
+const V& BoxSubset<V,M>::maxValues() const
+{
+  return m_maxValues;
+}
+
+// I/O method
+template <class V, class M>
+void BoxSubset<V,M>::print(std::ostream& os) const
+{
+  os << "In BoxSubset<V,M>::print()"
+     << ": m_minValues = " << m_minValues
+     << ", m_maxValues = " << m_maxValues
+     << ", m_volume = "    << m_volume
+     << std::endl;
+
+  return;
+}
+
+}  // End namespace QUESO
