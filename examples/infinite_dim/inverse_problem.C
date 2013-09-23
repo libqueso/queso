80036584db5c503a80c85bca80b73af18c978b14
Damon McDougall <damon@ices.utexas.edu>
2013-09-22 21:13:30 -0500

    Add a working inf dim inv prob example

--- examples/infinite_dim/inverse_problem.C
+++ examples/infinite_dim/inverse_problem.C
@@ -1,54 +1,130 @@
-#include <uqLibMeshFunction.h>
-#include <uqLaplacianOperator.h>
-#include <uqEnvironment.h>
-#include <uqStatisticalInverseProblem.h>
+#include <iostream>
+#include <cmath>
+#include <string>
 
-int main(int argc, char **argv)
+// GSL includes
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+// Boost includes
+#include <boost/math/constants/constants.hpp>
+
+// LibMesh includes
+#include <libmesh/libmesh.h>
+#include <libmesh/mesh.h>
+#include <libmesh/mesh_generation.h>
+#include <libmesh/equation_systems.h>
+#include <libmesh/vector_value.h>
+#include <libmesh/exact_solution.h>
+#include <libmesh/exact_error_estimator.h>
+#include <libmesh/explicit_system.h>
+
+// QUESO includes
+#include <queso/FunctionOperatorBuilder.h>
+#include <queso/LibMeshFunction.h>
+#include <queso/LibMeshNegativeLaplacianOperator.h>
+#include <queso/InfiniteDimensionalGaussian.h>
+#include <queso/InfiniteDimensionalLikelihoodBase.h>
+#include <queso/InfiniteDimensionalMCMCSamplerOptions.h>
+#include <queso/InfiniteDimensionalMCMCSampler.h>
+
+// The likelihood object subclass
+class Likelihood : public QUESO::InfiniteDimensionalLikelihoodBase
 {
-  int num_times = 40;
-  double obs_var = 1e-2;
-  double burger_speed = 1.0;
-  double dt = 0.01;
+public:
+  Likelihood(double obs_stddev);
+  ~Likelihood();
+  virtual double evaluate(QUESO::FunctionBase & state);
+  gsl_rng *r;
+};
+
+Likelihood::Likelihood(double obs_stddev)
+  : QUESO::InfiniteDimensionalLikelihoodBase(obs_stddev)
+{
+  this->r = gsl_rng_alloc(gsl_rng_default);
+}
+
+Likelihood::~Likelihood()
+{
+  gsl_rng_free(this->r);
+}
+
+double Likelihood::evaluate(QUESO::FunctionBase & flow)
+{
+  // This is where you call your forward problem, make observations and compute
+  // the likelihood norm.  Here we'll just pretend we observe a zero with
+  // error.
+  const double obs_stddev = this->obs_stddev();
+  const double obs = gsl_ran_gaussian(this->r, obs_stddev);
+  return obs * obs / (2.0 * obs_stddev * obs_stddev);
+}
+
+int main(int argc, char **argv) {
+  unsigned int num_pts = 100;
+  unsigned int num_pairs = num_pts / 2.0;
+  const double alpha = 3.0;
+  const double beta = 10.0;
+  const double obs_stddev = 1e-3;
 
   MPI_Init(&argc, &argv);
 
-  // Set up the environment
-  uqFullEnvironmentClass *env = new uqFullEnvironmentClass(MPI_COMM_WORLD,
-      "my_input_file.in", "", NULL);
+  QUESO::FullEnvironment env(MPI_COMM_WORLD, "infinite_dim/inverse_options", "", NULL);
 
-  // Set up the prior -- only Gaussians are supported for now
-  // A gaussian prior is specified by a mean function and a covariance operator
+  libMesh::LibMeshInit init(argc, argv);
 
-  // Mean function
-  uqLibMeshFunction *mu = new uqLibMeshFunction();
-  // Maybe the default constructor should default to the zero function?
+  // Generate the mesh on which the samples live.
+  libMesh::Mesh mesh;
+  libMesh::MeshTools::Generation::build_line(mesh, num_pts, 0.0, 1.0, EDGE2);
 
-  // Inverse covariance operator: e.g., the inverse laplacian to the 3
-  uqNegativeLaplacianOperator *C = new uqNegativeLaplacianOperator(-3.0);
+  // Use a helper object to define some of the properties of our samples
+  QUESO::FunctionOperatorBuilder fobuilder;
+  fobuilder.order = "FIRST";
+  fobuilder.family = "LAGRANGE";
+  fobuilder.num_req_eigenpairs = num_pairs;
 
-  // Set up the Gaussian measure.
-  uqGaussianRF *prior = new GAUSSIAN_MEASURE(mu, C);
+  // Define the mean of the prior
+  QUESO::LibMeshFunction mean(fobuilder, mesh);
 
-  // Set up the likelihood. It takes a field
-  Likelihood *likelihood = new Likelihood(*env, true_initial_condition,
-      burger_speed, num_times, dt, obs_var);
-  likelihood->synthesize_data();
+  // Define the precision operator of the prior
+  QUESO::LibMeshNegativeLaplacianOperator precision(fobuilder, mesh);
 
-  // Set up the inverse problem
-  uqSipOptionsValuesClass opts;
-  uqStatisticalInverseProblemClass *ip = new
-    uqStatisticalInverseProblemClass("", &opts, *prior, *likelihood);
+  // Define the prior measure
+  QUESO::InfiniteDimensionalGaussian mu(env, mean, precision, alpha, beta);
 
-  // MCMC seed
-  uqLibMeshFunction initialVals();  // Zero as the MCMC seed
-  ip->solveWithBayesMetropolisHastings (NULL, initialVals);
+  // Create likelihood object
+  Likelihood llhd(obs_stddev);
 
-  delete env;
-  delete mu;
-  delete C;
-  delete prior;
-  delete likelihood;
-  MPI_Finalize();
+  // The worst hack in the world ever
+  // boost::shared_ptr<LibMeshFunction> lm_draw(boost::static_pointer_cast<LibMeshFunction>(mu.draw()));
+  // std::cout << "before zero" << std::endl;
+  // lm_draw->equation_systems->get_system<ExplicitSystem>("Function").solution->zero();
+  // std::cout << "after zero" << std::endl;
+
+  // Create the options helper object that determines what options to pass to
+  // the sampler
+  QUESO::InfiniteDimensionalMCMCSamplerOptions opts(env, "");
+
+  // Set the number of iterations to do
+  opts.m_num_iters = 1000;
+
+  // Set the frequency with which we save samples
+  opts.m_save_freq = 10;
+
+  // Set the RWMH step size
+  opts.m_rwmh_step = 0.1;
+
+  // Construct the sampler, and set the name of the output file (will only
+  // write HDF5 files)
+  QUESO::InfiniteDimensionalMCMCSampler s(env, mu, llhd, &opts);
+
+  for (unsigned int i = 0; i < opts.m_num_iters; i++) {
+    s.step();
+    if (i % 100 == 0) {
+      std::cout << "sampler iteration: " << i << std::endl;
+      std::cout << "avg acc prob is: " << s.avg_acc_prob() << std::endl;
+      std::cout << "l2 norm is: " << s.llhd_val() << std::endl;
+    }
+  }
 
   return 0;
 }
