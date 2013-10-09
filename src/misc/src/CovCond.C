94c593895d879d71c62f17679fda38f1bd8d7e04
Damon McDougall <damon@ices.utexas.edu>
2013-10-09 13:11:51 -0500

    Pull out implementation of CovCond from header file

--- src/misc/src/CovCond.C
+++ src/misc/src/CovCond.C
@@ -26,15 +26,49 @@
 //
 //--------------------------------------------------------------------------
 
-#ifndef UQ_COV_COND_H
-#define UQ_COV_COND_H
+#include <queso/CovCond.h>
 
 namespace QUESO {
 
 template <class V, class M>
-void CovCond(double condNumber, const V& direction, M & covMatrix,
-    M & precMatrix);
+void CovCond(double condNumber, const V & direction, M & covMatrix,
+    M & precMatrix)
+{
+  //std::cout << "Entering CovCond()"
+  //          << std::endl;
+
+  V v1(direction);
+  //std::cout << "In CovCond(), v1 contents are:"
+  //          << std::endl
+  //          << v1
+  //          << std::endl;
+
+  V v2(direction,condNumber,1.0); // MATLAB linspace
+  v2.cwInvert();
+  v2.sort();
+  //std::cout << "In CovCond(), v2 contents are:"
+  //          << std::endl
+  //          << v2
+  //          << std::endl;
+
+  double v1Norm2 = v1.norm2();
+  if (v1[0] >=0) v1[0] += v1Norm2;
+  else           v1[0] -= v1Norm2;
+  double v1Norm2Sq = v1.norm2Sq();
+
+  M Z(direction,1.0);
+  Z -= (2./v1Norm2Sq) * matrixProduct(v1,v1);
+  //std::cout << "In CovCond(), Z contents are:"
+  //          << std::endl
+  //          << Z
+  //          << std::endl;
+
+  M Zt(Z.transpose());
+  covMatrix  = Z * leftDiagScaling(v2,   Zt);
+  precMatrix = Z * leftDiagScaling(1./v2,Zt);
+
+  //std::cout << "Leaving CovCond()"
+  //          << std::endl;
+}
 
 }  // End namespace QUESO
-
-#endif // UQ_COV_COND_H
