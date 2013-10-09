585e58e9a1246f4d15923cf3b27538711c1118d2
Damon McDougall <damon@ices.utexas.edu>
2013-10-09 13:58:27 -0500

    Pull out implementation of ArrayOfOneDGrids from header

--- src/misc/inc/ArrayOfOneDGrids.h
+++ src/misc/inc/ArrayOfOneDGrids.h
@@ -30,10 +30,11 @@
 #define UQ_ARRAY_OF_ONE_D_GRIDS_H
 
 #include <queso/OneDGrid.h>
+#include <queso/VectorSpace.h>
 
 namespace QUESO {
 
-/*!\file uqArrayOfOneDGrids.h
+/*!\file ArrayOfOneDGrids.h
  * \brief Class to accommodate arrays of one-dimensional grid.
  * 
  * \class ArrayOfOneDGrids
@@ -105,141 +106,6 @@ private:
   V* m_maxPositions;
 
 };
-// Default constructor -------------------------------------------------
-template <class V, class M>
-ArrayOfOneDGrids<V,M>::ArrayOfOneDGrids(
-  const char*                    prefix,
-  const VectorSpace<V,M>& rowSpace)
-  :
-  m_env         (rowSpace.env()    ),
-  m_prefix      ((std::string)(prefix)+""),
-  m_rowSpace    (rowSpace          ),
-  m_oneDGrids   (m_rowSpace.map(),1),
-  m_sizes       (NULL),
-  m_minPositions(NULL),
-  m_maxPositions(NULL)
-{
-  for (unsigned int i = 0; i < (unsigned int) m_oneDGrids.MyLength(); ++i) {
-    m_oneDGrids(i,0) = NULL;
-  }
-}
-// Destructor ----------------------------------------------------------
-template <class V, class M>
-ArrayOfOneDGrids<V,M>::~ArrayOfOneDGrids()
-{
-  if (m_maxPositions) delete m_maxPositions;
-  if (m_minPositions) delete m_minPositions;
-  if (m_sizes       ) delete m_sizes;
-
-  for (unsigned int i = 0; i < (unsigned int) m_oneDGrids.MyLength(); ++i) {
-    if (m_oneDGrids(i,0)) delete m_oneDGrids(i,0);
-  }
-}
-// Property methods-----------------------------------------------------
-template <class V, class M>
-const VectorSpace<V,M>&
-ArrayOfOneDGrids<V,M>::rowSpace() const
-{
-  return m_rowSpace;
-}
-//----------------------------------------------------------------------
-template <class V, class M>
-const V&
-ArrayOfOneDGrids<V,M>::sizes() const
-{
-  UQ_FATAL_TEST_MACRO(m_sizes == NULL,
-                      m_env.worldRank(),
-                      "ArrayOfOneDGrids<T>::sizes()",
-                      "sizes is still NULL");
-
-  return *m_sizes;
-}
-//----------------------------------------------------------------------
-template <class V, class M>
-const V&
-ArrayOfOneDGrids<V,M>::minPositions() const
-{
-  UQ_FATAL_TEST_MACRO(m_minPositions == NULL,
-                      m_env.worldRank(),
-                      "ArrayOfOneDGrids<T>::minPositions()",
-                      "minPositions is still NULL");
-
-  return *m_minPositions;
-}
-//----------------------------------------------------------------------
-template <class V, class M>
-const V&
-ArrayOfOneDGrids<V,M>::maxPositions() const
-{
-  UQ_FATAL_TEST_MACRO(m_maxPositions == NULL,
-                      m_env.worldRank(),
-                      "ArrayOfOneDGrids<T>::maxPositions()",
-                      "maxPositions is still NULL");
-
-  return *m_maxPositions;
-}
-//----------------------------------------------------------------------
-template <class V, class M>
-void
-ArrayOfOneDGrids<V,M>::setUniformGrids(
-  const V& sizesVec,
-  const V& minPositionsVec,
-  const V& maxPositionsVec)
-{
-  if (m_sizes        == NULL) m_sizes        = new V(sizesVec);
-  else                       *m_sizes        = sizesVec;
-
-  if (m_minPositions == NULL) m_minPositions = new V(minPositionsVec);
-  else                       *m_minPositions = minPositionsVec;
-
-  if (m_maxPositions == NULL) m_maxPositions = new V(maxPositionsVec);
-  else                       *m_maxPositions = maxPositionsVec;
-
-  char strI[65];
-  for (unsigned int i = 0; i < (unsigned int) m_oneDGrids.MyLength(); ++i) {
-    sprintf(strI,"%u_",i);
-    m_oneDGrids(i,0) = new UniformOneDGrid<double>(m_env,
-                                                          (m_prefix+strI).c_str(),
-                                                          (unsigned int) sizesVec[i],
-                                                          minPositionsVec[i],
-                                                          maxPositionsVec[i]);
-  }
-
-  return;
-}
-//----------------------------------------------------------------------
-template <class V, class M>
-const BaseOneDGrid<double>&
-ArrayOfOneDGrids<V,M>::grid(unsigned int rowId) const
-{
-  UQ_FATAL_TEST_MACRO(rowId >= m_rowSpace.dimLocal(),
-                      m_env.worldRank(),
-                      "ArrayOfOneDUnformGrids<T>::grid()",
-                      "rowId is out of range");
-
-  ArrayOfOneDGrids<V,M>* tmp = const_cast<ArrayOfOneDGrids<V,M>*>(this);
-  return *(tmp->m_oneDGrids(rowId,0));
-}
-// I/O methods----------------------------------------------------------
-template <class V, class M>
-void
-ArrayOfOneDGrids<V,M>::print(std::ostream& os) const
-{
-  ArrayOfOneDGrids<V,M>* tmp = const_cast<ArrayOfOneDGrids<V,M>*>(this);
-  for (unsigned int i = 0; i < (unsigned int) m_oneDGrids.MyLength(); ++i) {
-    os << *(tmp->m_oneDGrids(i,0))
-       << std::endl;
-  }
-
-  return;
-}
-//----------------------------------------------------------------------
-template <class V, class M>
-std::ostream& operator<< (std::ostream& os, const ArrayOfOneDGrids<V,M>& obj)
-{
-  obj.print(os);
-  return os;
-}
 
 }  // End namespace QUESO
 
