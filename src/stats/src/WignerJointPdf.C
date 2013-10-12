21c3dceff79bcba6cd41f11ecfbb40e328f182f6
Damon McDougall <damon@ices.utexas.edu>
2013-10-12 00:53:42 -0500

    Add separated impl of WignerJointPdf

---
+++ src/stats/src/WignerJointPdf.C
@@ -0,0 +1,135 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#include <queso/WignerJointPdf.h>
+
+namespace QUESO {
+
+// Constructor -------------------------------------
+template<class V,class M>
+WignerJointPdf<V,M>::WignerJointPdf(
+  const char*                  prefix,
+  const VectorSet<V,M>& domainSet,
+  const V&                     centerPos,
+  double                       radius)
+  :
+  BaseJointPdf<V,M>(((std::string)(prefix)+"uni").c_str(),
+         domainSet),
+  m_centerPos(new V(centerPos)),
+  m_radius   (radius)    
+{
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 54)) {
+    *m_env.subDisplayFile() << "Entering WignerJointPdf<V,M>::constructor()"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+
+  UQ_FATAL_TEST_MACRO(m_radius <= 0.,
+                      m_env.worldRank(),
+                      "WignerJointPdf<V,M>::constructor()",
+                      "invalid radius");
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 54)) {
+    *m_env.subDisplayFile() << "Leaving WignerJointPdf<V,M>::constructor()"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+}
+// Destructor --------------------------------------
+template<class V,class M>
+WignerJointPdf<V,M>::~WignerJointPdf()
+{
+  delete m_centerPos;
+}
+// Math methods-------------------------------------
+template<class V, class M>
+double
+WignerJointPdf<V,M>::actualValue(
+  const V& domainVector,
+  const V* domainDirection,
+        V* gradVector,
+        M* hessianMatrix,
+        V* hessianEffect) const
+{
+  UQ_FATAL_TEST_MACRO(domainVector.sizeLocal() != this->m_domainSet.vectorSpace().dimLocal(),
+                      m_env.worldRank(),
+                      "WignerJointPdf<V,M>::actualValue()",
+                      "invalid input");
+
+  if (gradVector   ) *gradVector     = m_domainSet.vectorSpace().zeroVector();
+  if (hessianMatrix) *hessianMatrix *= 0.;
+  if (hessianEffect) *hessianEffect  = m_domainSet.vectorSpace().zeroVector();
+
+  double returnValue = 0.;
+  double distanceRatio = (domainVector - *m_centerPos).norm2()/m_radius;
+  if (distanceRatio < 1.) {
+    returnValue = 2.*m_radius*m_radius*sqrt(1. - distanceRatio*distanceRatio)/M_PI;
+  }
+  returnValue *= exp(m_logOfNormalizationFactor); // [PDF-09]
+
+  return returnValue;
+}
+//--------------------------------------------------
+template<class V, class M>
+double
+WignerJointPdf<V,M>::lnValue(
+  const V& domainVector,
+  const V* domainDirection,
+        V* gradVector,
+        M* hessianMatrix,
+        V* hessianEffect) const
+{
+  if (gradVector   ) *gradVector     = m_domainSet.vectorSpace().zeroVector();
+  if (hessianMatrix) *hessianMatrix *= 0.;
+  if (hessianEffect) *hessianEffect  = m_domainSet.vectorSpace().zeroVector();
+
+  // No need to add m_logOfNormalizationFactor because 'actualValue()' is called [PDF-09]
+  return log(this->actualValue(domainVector,domainDirection,gradVector,hessianMatrix,hessianEffect));
+}
+//--------------------------------------------------
+template<class V, class M>
+double
+WignerJointPdf<V,M>::computeLogOfNormalizationFactor(unsigned int numSamples, bool updateFactorInternally) const
+{
+  double value = 0.;
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 2)) {
+    *m_env.subDisplayFile() << "Entering WignerJointPdf<V,M>::computeLogOfNormalizationFactor()"
+                            << std::endl;
+  }
+  value = BaseJointPdf<V,M>::commonComputeLogOfNormalizationFactor(numSamples, updateFactorInternally);
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 2)) {
+    *m_env.subDisplayFile() << "Leaving WignerJointPdf<V,M>::computeLogOfNormalizationFactor()"
+                            << ", m_logOfNormalizationFactor = " << m_logOfNormalizationFactor
+                            << std::endl;
+  }
+
+  return value;
+}
+
+}  // End namespace QUESO
