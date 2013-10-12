9f2bc3c2c0e6671f5e34b19292f06d76a76ed2b9
Damon McDougall <damon@ices.utexas.edu>
2013-10-12 00:52:19 -0500

    Add separated impl of InverseGammaJointPdf

---
+++ src/stats/inc/InverseGammaJointPdf.h
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#ifndef UQ_INVGAMMA_JOINT_PROB_DENSITY_H
+#define UQ_INVGAMMA_JOINT_PROB_DENSITY_H
+
+#include <cmath>
+
+#include <boost/math/special_functions.hpp> // for Boost isnan. Note parentheses are important in function call.
+
+#include <queso/JointPdf.h>
+#include <queso/Environment.h>
+#include <queso/ScalarFunction.h>
+#include <queso/BoxSubset.h>
+
+namespace QUESO {
+
+//*****************************************************
+// InverseGamma probability density class [PDF-07]
+//*****************************************************
+/*!
+ * \class InverseGammaJointPdf
+ * \brief A class for handling Inverse Gamma joint PDFs.
+ *
+ * This class allows the mathematical definition of an Inverse Gamma Joint PDF.*/
+
+template<class V, class M>
+class InverseGammaJointPdf : public BaseJointPdf<V,M> {
+public:
+    //! @name Constructor/Destructor methods
+  //@{
+  //! Constructor
+  /*! Constructs a new object of the class, given a prefix, the domain set, and the parameters
+   * \c alpha and \c beta of the Inverse Gamma PDF.  */
+  InverseGammaJointPdf(const char*                  prefix,
+                              const VectorSet<V,M>& domainSet,
+                              const V&                     alpha,
+                              const V&                     beta);
+  //! Destructor
+ ~InverseGammaJointPdf();
+ //@}
+
+   //! @name Math methods
+  //@{
+  //! Actual value of the Gamma PDF.
+  /*! This routine calls method lnValue() and returns the exponent of the returning value of such method.*/
+  double actualValue(const V& domainVector, const V* domainDirection, V* gradVector, M* hessianMatrix, V* hessianEffect) const;
+  
+  //! TODO: Logarithm of the value of the Gamma PDF.
+  /*! \todo: implement me!*/
+  double lnValue    (const V& domainVector, const V* domainDirection, V* gradVector, M* hessianMatrix, V* hessianEffect) const;
+  
+  //! Computes the logarithm of the normalization factor.
+  /*! This routine calls BaseJointPdf::commonComputeLogOfNormalizationFactor().*/
+  double computeLogOfNormalizationFactor(unsigned int numSamples, bool updateFactorInternally) const;
+  //@}
+
+protected:
+  using BaseScalarFunction<V,M>::m_env;
+  using BaseScalarFunction<V,M>::m_prefix;
+  using BaseScalarFunction<V,M>::m_domainSet;
+  using BaseJointPdf<V,M>::m_normalizationStyle;
+  using BaseJointPdf<V,M>::m_logOfNormalizationFactor;
+
+  V m_alpha;
+  V m_beta;
+};
+
+}  // End namespace QUESO
+
+#endif // UQ_INVGAMMA_JOINT_PROB_DENSITY_H
---
+++ src/stats/src/InverseGammaJointPdf.C
@@ -0,0 +1,133 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#include <queso/InverseGammaJointPdf.h>
+
+namespace QUESO {
+
+// Constructor -------------------------------------
+template<class V,class M>
+InverseGammaJointPdf<V,M>::InverseGammaJointPdf(
+  const char*                  prefix,
+  const VectorSet<V,M>& domainSet,
+  const V&                     alpha,
+  const V&                     beta)
+  :
+  BaseJointPdf<V,M>(((std::string)(prefix)+"uni").c_str(),domainSet),
+  m_alpha(alpha),
+  m_beta (beta)
+{
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 54)) {
+    *m_env.subDisplayFile() << "Entering InverseGammaJointPdf<V,M>::constructor()"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 54)) {
+    *m_env.subDisplayFile() << "Leaving InverseGammaJointPdf<V,M>::constructor()"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+}
+// Destructor --------------------------------------
+template<class V,class M>
+InverseGammaJointPdf<V,M>::~InverseGammaJointPdf()
+{
+}
+// Math methods-------------------------------------
+template<class V, class M>
+double
+InverseGammaJointPdf<V,M>::actualValue(
+  const V& domainVector,
+  const V* domainDirection,
+        V* gradVector,
+        M* hessianMatrix,
+        V* hessianEffect) const
+{
+  UQ_FATAL_TEST_MACRO(domainVector.sizeLocal() != this->m_domainSet.vectorSpace().dimLocal(),
+                      m_env.worldRank(),
+                      "InverseGammaJointPdf<V,M>::actualValue()",
+                      "invalid input");
+
+  UQ_FATAL_TEST_MACRO((domainDirection || gradVector || hessianMatrix || hessianEffect),
+                      m_env.worldRank(),
+                      "InverseGammaJointPdf<V,M>::actualValue()",
+                      "incomplete code for gradVector, hessianMatrix and hessianEffect calculations");
+
+  // No need to multiply by exp(m_logOfNormalizationFactor) because 'lnValue()' is called [PDF-07]
+  return exp(this->lnValue(domainVector,domainDirection,gradVector,hessianMatrix,hessianEffect));
+}
+//--------------------------------------------------
+template<class V, class M>
+double
+InverseGammaJointPdf<V,M>::lnValue(
+  const V& domainVector,
+  const V* domainDirection,
+        V* gradVector,
+        M* hessianMatrix,
+        V* hessianEffect) const
+{
+  UQ_FATAL_TEST_MACRO((domainDirection || gradVector || hessianMatrix || hessianEffect),
+                      m_env.worldRank(),
+                      "InverseGammaJointPdf<V,M>::lnValue()",
+                      "incomplete code for gradVector, hessianMatrix and hessianEffect calculations");
+
+  double result = 0.;
+  for (unsigned int i = 0; i < domainVector.sizeLocal(); ++i) {
+    result -= (m_alpha[i]+1.)*log(domainVector[i]);
+    result -= m_beta[i]/domainVector[i];
+    if (m_normalizationStyle == 0) {
+      // Code needs to be done yet
+    }
+  }
+  result += m_logOfNormalizationFactor; // [PDF-07]
+
+  return result;
+}
+//--------------------------------------------------
+template<class V, class M>
+double
+InverseGammaJointPdf<V,M>::computeLogOfNormalizationFactor(unsigned int numSamples, bool updateFactorInternally) const
+{
+  double value = 0.;
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 2)) {
+    *m_env.subDisplayFile() << "Entering InverseGammaJointPdf<V,M>::computeLogOfNormalizationFactor()"
+                            << std::endl;
+  }
+  value = BaseJointPdf<V,M>::commonComputeLogOfNormalizationFactor(numSamples, updateFactorInternally);
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 2)) {
+    *m_env.subDisplayFile() << "Leaving InverseGammaJointPdf<V,M>::computeLogOfNormalizationFactor()"
+                            << ", m_logOfNormalizationFactor = " << m_logOfNormalizationFactor
+                            << std::endl;
+  }
+
+  return value;
+}
+
+}  // End namespace QUESO
