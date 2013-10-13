5fa443cbe2d004352ec57710a37e6968363ade3a
Damon McDougall <damon@ices.utexas.edu>
2013-10-13 12:47:33 -0500

    Add separated implementation of GaussianVectorMdf

---
+++ src/stats/inc/GaussianVectorMdf.h
@@ -0,0 +1,101 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#ifndef UQ_GAUSSIAN_VECTOR_MARGINAL_DENSITY_FUNCTION_H
+#define UQ_GAUSSIAN_VECTOR_MARGINAL_DENSITY_FUNCTION_H
+
+#include <queso/VectorMdf.h>
+#include <queso/ArrayOfOneDGrids.h>
+#include <queso/ArrayOfOneDTables.h>
+#include <queso/Environment.h>
+#include <math.h>
+
+namespace QUESO {
+
+//*****************************************************
+// Gaussian marginal density function class
+//*****************************************************
+/*! 
+ * \class GaussianVectorMdf
+ * \brief TODO: A class for handling Gaussian MDFs.
+ *
+ * This class \b will implement a Gaussian vector marginal density function function (MDF).
+ * \todo: Implement me! */
+
+template<class V, class M>
+class GaussianVectorMdf : public BaseVectorMdf<V,M> {
+public:
+    //! @name Constructor/Destructor methods
+  //@{ 
+  //! TODO: Constructor.
+  /*! \todo: implement me! This method calls commonConstructor() which is not yet implemented.
+   * Instantiates an object of the class given a prefix, the domain set, and the domain mean 
+   * and expected values. */
+  GaussianVectorMdf(const char*                    prefix,
+                           const VectorSet<V,M>& domainSet,
+                           const V&                       domainExpectedValues,
+                           const V&                       domainVarianceValues);
+  //! TODO: Constructor.
+  /*! \todo: implement me! This method calls commonConstructor() which is not yet implemented.
+   * Instantiates an object of the class given a prefix, the domain set, and the domain mean 
+   * and covariance matrix. */
+  GaussianVectorMdf(const char*                    prefix,
+                           const VectorSet<V,M>& domainSet,
+                           const V&                       domainExpectedValues,
+                           const M&                       covMatrix);
+  //! Destructor
+  ~GaussianVectorMdf();
+  //@}
+
+  //! @name Mathematical method
+  //@{
+  //! TODO: Returns the values of the vector CDF at each element of \c paramValues. 
+  /*! \todo: implement me!*/
+  void values(const V& paramValues, V& mdfVec) const;
+  //@}
+  
+  //! @name I/O method
+  //@{ 
+  //! TODO: Prints the vector CDF. 
+  /*! \todo: implement me!*/
+  void print (std::ostream& os)                const;
+  //@}
+
+protected:
+  const M*                         m_covMatrix;
+
+  using BaseVectorMdf<V,M>::m_env;
+  using BaseVectorMdf<V,M>::m_prefix;
+  using BaseVectorMdf<V,M>::m_domainSet;
+
+  void commonConstructor();
+};
+
+}  // End namespace QUESO
+
+#endif // UQ_GAUSSIAN_VECTOR_MARGINAL_DENSITY_FUNCTION_H
---
+++ src/stats/src/GaussianVectorMdf.C
@@ -0,0 +1,121 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#include <queso/GaussianVectorMdf.h>
+
+namespace QUESO {
+
+// Constructor -------------------------------------
+template<class V,class M>
+GaussianVectorMdf<V,M>::GaussianVectorMdf(
+  const char*                    prefix,
+  const VectorSet<V,M>& domainSet,
+  const V&                       domainExpectedValues,
+  const V&                       domainVarianceValues)
+  :
+  BaseVectorMdf<V,M>(prefix,domainSet),
+  m_covMatrix              (m_domainSet.newDiagMatrix(domainVarianceValues*domainVarianceValues))
+{
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 5)) {
+    *m_env.subDisplayFile() << "Entering GaussianVectorMdf<V,M>::constructor() [1]"
+                           << ": prefix = " << m_prefix
+                           << std::endl;
+  }
+
+  commonConstructor();
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 5)) {
+    *m_env.subDisplayFile() << "Leaving GaussianVectorMdf<V,M>::constructor() [1]"
+                           << ": prefix = " << m_prefix
+                           << std::endl;
+  }
+}
+// Constructor -------------------------------------
+template<class V,class M>
+GaussianVectorMdf<V,M>::GaussianVectorMdf(
+  const char*                    prefix,
+  const VectorSet<V,M>& domainSet,
+  const V&                       domainExpectedValues,
+  const M&                       covMatrix)
+  :
+  BaseVectorMdf<V,M>(prefix,domainSet),
+  m_covMatrix              (new M(covMatrix))
+{
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 5)) {
+    *m_env.subDisplayFile() << "Entering GaussianVectorMdf<V,M>::constructor() [2]"
+                           << ": prefix = " << m_prefix
+                           << std::endl;
+  }
+
+  commonConstructor();
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 5)) {
+    *m_env.subDisplayFile() << "Leaving GaussianVectorMdf<V,M>::constructor() [2]"
+                           << ": prefix = " << m_prefix
+                           << std::endl;
+  }
+}
+// Destructor -------------------------------------
+template<class V,class M>
+GaussianVectorMdf<V,M>::~GaussianVectorMdf()
+{
+  delete m_covMatrix;
+}
+// Math method -------------------------------------
+template<class V, class M>
+void
+GaussianVectorMdf<V,M>::values(
+  const V& paramValues,
+        V& mdfVec) const
+{
+  UQ_FATAL_TEST_MACRO(true,
+                      m_env.worldRank(),
+                      "GaussianVectorMdf<V,M>::mdfVec()",
+                      "incomplete code");
+  return;
+}
+// I/O method --------------------------------------
+template <class V, class M>
+void
+GaussianVectorMdf<V,M>::print(std::ostream& os) const
+{
+  return;
+}
+// Protected member function------------------------
+template<class V,class M>
+void
+GaussianVectorMdf<V,M>::commonConstructor()
+{
+  UQ_FATAL_TEST_MACRO(true,
+                      m_env.worldRank(),
+                      "GaussianVectorMdf<V,M>::commonConstructor()",
+                      "incomplete code");
+  return;
+}
+
+}  // End namespace QUESO
