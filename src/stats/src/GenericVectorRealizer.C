bd8b67ae085237e33348aaaeb0a6ff265c334892
Damon McDougall <damon@ices.utexas.edu>
2013-10-14 00:30:38 -0500

    Add separate implementation of GenericVectorRealizer

---
+++ src/stats/src/GenericVectorRealizer.C
@@ -0,0 +1,76 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#include <queso/GenericVectorRealizer.h>
+#include <queso/GslVector.h>
+#include <queso/GslMatrix.h>
+
+namespace QUESO {
+
+// Default constructor -----------------------------
+template<class V, class M>
+GenericVectorRealizer<V,M>::GenericVectorRealizer(
+  const char*                  prefix,
+  const VectorSet<V,M>& unifiedImageSet,
+  unsigned int                 subPeriod,
+  double (*routinePtr)(const void* routineDataPtr, V& nextParamValues),
+  const void* routineDataPtr)
+  :
+  BaseVectorRealizer<V,M>(((std::string)(prefix)+"gen").c_str(),unifiedImageSet,subPeriod),
+  m_routinePtr    (routinePtr),
+  m_routineDataPtr(routineDataPtr)
+{
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 5)) {
+    *m_env.subDisplayFile() << "Entering GenericVectorRealizer<V,M>::constructor()"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 5)) {
+    *m_env.subDisplayFile() << "Leaving GenericVectorRealizer<V,M>::constructor()"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+}
+// Destructor --------------------------------------
+template<class V, class M>
+GenericVectorRealizer<V,M>::~GenericVectorRealizer()
+{
+}
+// Realization-related methods----------------------
+template<class V, class M>
+void
+GenericVectorRealizer<V,M>::realization(V& nextValues) const
+{
+  m_routinePtr(m_routineDataPtr,nextValues);
+  return;
+}
+
+}  // End namespace QUESO
+
+template class QUESO::GenericVectorRealizer<QUESO::GslVector, QUESO::GslMatrix>;
