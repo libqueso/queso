459f02d21d2de16d4667607063236000730d3fc4
Damon McDougall <damon@ices.utexas.edu>
2013-10-14 14:59:56 -0500

    Add separated implementation of LogNormalVectorRV

---
+++ src/stats/inc/LogNormalVectorRV.h
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#ifndef UQ_LOGNORM_VECTOR_RV_H
+#define UQ_LOGNORM_VECTOR_RV_H
+
+#include <queso/VectorRV.h>
+#include <queso/VectorSpace.h>
+#include <queso/JointPdf.h>
+#include <queso/VectorRealizer.h>
+#include <queso/VectorCdf.h>
+#include <queso/VectorMdf.h>
+#include <queso/SequenceOfVectors.h>
+#include <queso/InfoTheory.h>
+#include <gsl/gsl_sf_psi.h> // todo: take specificity of gsl_, i.e., make it general (gsl or boost or etc)
+
+namespace QUESO {
+
+//*****************************************************
+// LogNormal class [RV-10]
+//*****************************************************
+/*!
+ * \class LogNormalVectorRV
+ * \brief A class representing a LogNormal vector RV.
+ * 
+ * This class allows the user to compute the value of a LogNormal PDF and to generate realizations
+ * (samples) from it.\n
+ * 
+ * The probability density function of a log-normal distribution is: 
+ *   \f[ f(x|\mu,\sigma) = \frac{1}{x \sigma \sqrt{2 \pi}}\, e^{-\frac{(\ln x - \mu)^2}{2\sigma^2}}, x>0 \f]
+ * where  the parameters denoted \f$ \mu \f$ and \f$ \sigma \f$ are, respectively, the mean and standard 
+ * deviation of the  variable's natural logarithm; and \c x>0.  */
+
+template<class V, class M>
+class LogNormalVectorRV : public BaseVectorRV<V,M> {
+public:
+    //! @name Constructor/Destructor methods
+  //@{
+  //! Constructor  
+  /*! Construct a LogNormal vector RV with mean \c lawExpVector and diagonal covariance matrix
+   * \c lawVarVector whose variates live in \c imageSet.*/
+  LogNormalVectorRV(const char*                  prefix,
+                           const VectorSet<V,M>& imageSet,
+                           const V&                     lawExpVector,
+                           const V&                     lawVarVector);
+  
+  //! Virtual destructor
+  virtual ~LogNormalVectorRV();
+  //@}
+ 
+  //! @name I/O methods
+  //@{
+  //! TODO: Prints the vector RV.
+  /*! \todo: implement me!*/
+  void print(std::ostream& os) const;
+  //@}
+
+private:
+  using BaseVectorRV<V,M>::m_env;
+  using BaseVectorRV<V,M>::m_prefix;
+  using BaseVectorRV<V,M>::m_imageSet;
+  using BaseVectorRV<V,M>::m_pdf;
+  using BaseVectorRV<V,M>::m_realizer;
+  using BaseVectorRV<V,M>::m_subCdf;
+  using BaseVectorRV<V,M>::m_unifiedCdf;
+  using BaseVectorRV<V,M>::m_mdf;
+};
+
+}  // End namespace QUESO
+
+#endif // UQ_LOGNORM_VECTOR_RV_H
---
+++ src/stats/src/LogNormalVectorRV.C
@@ -0,0 +1,148 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#include <queso/LogNormalVectorRV.h>
+#include <queso/GslVector.h>
+#include <queso/GslMatrix.h>
+
+namespace QUESO {
+
+// Constructor---------------------------------------
+template<class V, class M>
+LogNormalVectorRV<V,M>::LogNormalVectorRV(
+  const char*                  prefix,
+  const VectorSet<V,M>& imageSet,
+  const V&                     lawExpVector,
+  const V&                     lawVarVector)
+  :
+  BaseVectorRV<V,M>(((std::string)(prefix)+"gau").c_str(),imageSet)
+{
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 54)) {
+    *m_env.subDisplayFile() << "Entering LogNormalVectorRV<V,M>::constructor() [1]"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+
+// begin kemelli 2013-April-23 -------------------------- 
+// LogNormal dist is defined only in (0,inf)
+
+  const BoxSubset<V,M>* imageBox = dynamic_cast<const BoxSubset<V,M>* >(&imageSet);
+  double smallerOfMaxValues = imageBox->maxValues().getMinValue();	
+  double smallerOfMinValues = imageBox->minValues().getMinValue();
+    
+ if( smallerOfMinValues < 0 ) 
+ {		
+   std::cerr << "In LogNormalVectorRV<V,M>::constructor()\n" 
+       << "LogNormal distribution is only defined in (0, infinity).\n"
+       << "The data provided is: \n"
+       << *imageBox 
+         << "Sampling will not cover all interval.\n"   
+         << std::endl;
+
+
+    UQ_FATAL_TEST_MACRO(smallerOfMaxValues < 0,
+                      m_env.worldRank(),
+                      "LogNormalVectorRealizer<V,M>::constructor()",
+                      "invalid input: LogNormal distribution is only defined in (0, infinity), and min(m_maxValues)<0");      
+              
+ }	
+// end kemelli 2013-April-23 --------------------------
+
+  m_pdf = new LogNormalJointPdf<V,M>(m_prefix.c_str(),
+                                            m_imageSet,
+                                            lawExpVector,
+                                            lawVarVector);
+
+  M lowerCholLawCovMatrix(lawVarVector);
+  int iRC = lowerCholLawCovMatrix.chol();
+  lowerCholLawCovMatrix.zeroUpper(false);
+  if (iRC) {
+    std::cerr << "In LogNormalVectorRV<V,M>::constructor() [1]: chol failed, will use svd\n";
+    if (m_env.subDisplayFile()) {
+      *m_env.subDisplayFile() << "In LogNormalVectorRV<V,M>::constructor() [1]: chol failed; will use svd; lawVarVector contents are\n";
+      *m_env.subDisplayFile() << lawVarVector; // FIX ME: might demand parallelism
+      *m_env.subDisplayFile() << std::endl;
+    }
+    M matLaw(lawVarVector);
+    M matU  (lawVarVector);
+    M matVt (m_imageSet.vectorSpace().zeroVector());
+    V vecS  (m_imageSet.vectorSpace().zeroVector());
+    iRC = matLaw.svd(matU,vecS,matVt);
+    UQ_FATAL_TEST_MACRO(iRC,
+                        m_env.worldRank(),
+                        "LogNormalVectorRV<V,M>::constructor() [1]",
+                        "Cholesky decomposition of covariance matrix failed.");
+
+    vecS.cwSqrt();
+    m_realizer = new LogNormalVectorRealizer<V,M>(m_prefix.c_str(),
+                                                         m_imageSet,
+                                                         lawExpVector,
+                                                         matU,
+                                                         vecS, // already square rooted
+                                                         matVt);
+  }
+  else {
+    m_realizer = new LogNormalVectorRealizer<V,M>(m_prefix.c_str(),
+                                                        m_imageSet,
+                                                        lawExpVector,
+                                                        lowerCholLawCovMatrix);
+  }
+
+  m_subCdf     = NULL; // FIX ME: complete code
+  m_unifiedCdf = NULL; // FIX ME: complete code
+  m_mdf        = NULL; // FIX ME: complete code
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 54)) {
+    *m_env.subDisplayFile() << "Leaving LogNormalVectorRV<V,M>::constructor() [1]"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+}
+// Destructor ---------------------------------------
+template<class V, class M>
+LogNormalVectorRV<V,M>::~LogNormalVectorRV()
+{
+  delete m_mdf;
+  delete m_unifiedCdf;
+  delete m_subCdf;
+  delete m_realizer;
+  delete m_pdf;
+}
+// I/O methods---------------------------------------
+template <class V, class M>
+void
+LogNormalVectorRV<V,M>::print(std::ostream& os) const
+{
+  os << "LogNormalVectorRV<V,M>::print() says, 'Please implement me.'" << std::endl;
+  return;
+}
+
+
+}  // End namespace QUESO
+
+template class QUESO::LogNormalVectorRV<QUESO::GslVector,QUESO::GslMatrix>;
