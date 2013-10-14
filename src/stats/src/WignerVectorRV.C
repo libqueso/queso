137a6a4c4daa2cf42dd6fbb7a1b39809009fb818
Damon McDougall <damon@ices.utexas.edu>
2013-10-14 15:00:07 -0500

    Add separated implementation of WignerVectorRV

---
+++ src/stats/src/WignerVectorRV.C
@@ -0,0 +1,95 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+// 
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+#include <queso/WignerVectorRV.h>
+#include <queso/GslVector.h>
+#include <queso/GslMatrix.h>
+
+namespace QUESO {
+
+// Default constructor -----------------------------
+template<class V, class M>
+WignerVectorRV<V,M>::WignerVectorRV(
+  const char*                  prefix,
+  const VectorSet<V,M>& imageSet,
+  const V&                     centerPos,
+  double                       radius)
+  :
+  BaseVectorRV<V,M>(((std::string)(prefix)+"uni").c_str(),imageSet)
+{
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 54)) {
+    *m_env.subDisplayFile() << "Entering WignerVectorRV<V,M>::constructor()"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+
+  UQ_FATAL_TEST_MACRO(radius <= 0.,
+                      m_env.worldRank(),
+                      "WignerVectorRV<V,M>::constructor()",
+                      "invalid radius");
+
+  m_pdf        = new WignerJointPdf<V,M>(m_prefix.c_str(),
+                                                m_imageSet,
+                                                centerPos,
+                                                radius);
+  m_realizer   = new WignerVectorRealizer<V,M>(m_prefix.c_str(),
+                                                      m_imageSet,
+                                                      centerPos,
+                                                      radius);
+  m_subCdf     = NULL; // FIX ME: complete code
+  m_unifiedCdf = NULL; // FIX ME: complete code
+  m_mdf        = NULL; // FIX ME: complete code
+
+  if ((m_env.subDisplayFile()) && (m_env.displayVerbosity() >= 54)) {
+    *m_env.subDisplayFile() << "Leaving WignerVectorRV<V,M>::constructor()"
+                            << ": prefix = " << m_prefix
+                            << std::endl;
+  }
+}
+// Constructor -------------------------------------
+template<class V, class M>
+WignerVectorRV<V,M>::~WignerVectorRV()
+{
+  delete m_mdf;
+  delete m_unifiedCdf;
+  delete m_subCdf;
+  delete m_realizer;
+  delete m_pdf;
+}
+//--------------------------------------------------
+template <class V, class M>
+void
+WignerVectorRV<V,M>::print(std::ostream& os) const
+{
+  os << "WignerVectorRV<V,M>::print() says, 'Please implement me.'" << std::endl;
+  return;
+}
+
+}  // End namespace QUESO
+
+template class QUESO::WignerVectorRV<QUESO::GslVector,QUESO::GslMatrix>;
