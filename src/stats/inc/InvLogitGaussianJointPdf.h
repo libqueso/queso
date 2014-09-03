ed69daa087c4ae3939366e90c41f398f63d65ee8
Damon McDougall <damon@ices.utexas.edu>
2014-09-02 22:50:43 -0500

    Add joint pdf docs for invlogitgaussian

--- src/stats/inc/InvLogitGaussianJointPdf.h
+++ src/stats/inc/InvLogitGaussianJointPdf.h
@@ -25,80 +25,116 @@
 #ifndef UQ_INVLOGIT_GAUSSIAN_JOINT_PROB_DENSITY_H
 #define UQ_INVLOGIT_GAUSSIAN_JOINT_PROB_DENSITY_H
 
-#include <cmath>
-
-#include <boost/math/special_functions.hpp> // for Boost isnan. Note parentheses are important in function call.
-
 #include <queso/JointPdf.h>
 #include <queso/ScalarFunction.h>
 #include <queso/BoxSubset.h>
 
 namespace QUESO {
 
-/*! 
+/*!
  * \class InvLogitGaussianJointPdf
- * \brief A class for handling Gaussian joint PDFs.
+ * \brief A class for handling hybrid (transformed) Gaussians with bounds
  *
- * This class allows the mathematical definition of a Gaussian Joint PDF.*/
+ * This class allows the mathematical definition of a Gaussian PDF in
+ * dimensions of a VectorSubset that are unbounded, and a \c logit transformed
+ * Gaussian PDF in dimensions that have a lower or upper (or both) bound.  For
+ * the bounded directions, the PDF is
+ *
+ * \f[
+ *   p(x) = \frac{1}{2 \sigma^2 \pi} \exp \left( - \frac12 (\text{logit} x - \mu)^2 \right) \text{logit}'(x), \quad x \in [a, b],
+ * \f]
+ *
+ * where \c logit is defined as
+ *
+ * \f[
+ *   \text{logit}(x) = \log \left( \frac{x - a}{b - x} \right), \quad x \in [a, b].
+ * \f]
+ */
 
 template<class V, class M>
 class InvLogitGaussianJointPdf : public BaseJointPdf<V,M> {
 public:
-    //! @name Constructor/Destructor methods
+  //! @name Constructor/Destructor methods
   //@{
-  //! Constructor 
-  /*! Constructs a new object, given a prefix and the domain of the PDF, a vector of mean
-   * values, \c lawExpVector, and a vector of covariance values \c lawVarVector (an 
-   * alternative representation for a diagonal covariance matrix).  */ 
-  InvLogitGaussianJointPdf(const char*                  prefix,
-                          const BoxSubset<V,M>& domainBoxSubset,
-                          const V&                     lawExpVector,
-                          const V&                     lawVarVector);
   //! Constructor
-  /*! Constructs a new object, given a prefix and the image set of the vector realizer, a
-   * vector of mean values, \c lawExpVector, and a covariance matrix, \c lawCovMatrix. */ 
-  InvLogitGaussianJointPdf(const char*                  prefix,
-                          const BoxSubset<V,M>& domainBoxSubset,
-                          const V&                     lawExpVector,
-                          const M&                     lawCovMatrix);
+  /*!
+   * Constructs a new object, given a prefix and the domain of the PDF, a
+   * vector of mean values, \c lawExpVector (for the Gaussian, not the
+   * transformed Gaussian), and a vector of covariance values
+   * \c lawVarVector (an alternative representation for a diagonal covariance
+   * matrix).
+   */
+  InvLogitGaussianJointPdf(const char * prefix,
+      const BoxSubset<V, M> & domainBoxSubset, const V & lawExpVector,
+      const V & lawVarVector);
+
+  //! Constructor
+  /*!
+   * Constructs a new object, given a prefix and the image set of the vector
+   * realizer, a vector of mean values, \c lawExpVector (for the Gaussian, not
+   * the transformed Gaussian), and a covariance matrix, \c lawCovMatrix.
+   */
+  InvLogitGaussianJointPdf(const char * prefix,
+      const BoxSubset<V, M> & domainBoxSubset, const V & lawExpVector,
+      const M & lawCovMatrix);
+
   //! Destructor
- ~InvLogitGaussianJointPdf();
- //@}
-  
+  ~InvLogitGaussianJointPdf();
+  //@}
+
   //! @name Math methods
   //@{
-    
-  //! Actual value of the Gaussian PDF:
-  /*! This method calls lnValue() and applies the exponential to it.*/
- double   actualValue       (const V& domainVector, const V* domainDirection, V* gradVector, M* hessianMatrix, V* hessianEffect) const;
-  
-  //! Logarithm of the value of the Gaussian PDF (scalar function).
- /*! The ln(value) comes from a summation of the Gaussian density:
-  * \f[ lnValue =- \sum_i \frac{1}{\sqrt{|covMatrix|} \sqrt{2 \pi}} exp(-\frac{(domainVector_i - lawExpVector_i)* covMatrix^{-1}* (domainVector_i - lawExpVector_i) }{2},  \f]
-  * where the \f$ covMatrix \f$ may recovered via \c this->lawVarVector(), in case of diagonal
-  * matrices or via \c this->m_lawCovMatrix, otherwise.*/
-  double   lnValue           (const V& domainVector, const V* domainDirection, V* gradVector, M* hessianMatrix, V* hessianEffect) const;
-  
-  //! Computes the logarithm of the normalization factor.
-  /*! This routine calls BaseJointPdf::commonComputeLogOfNormalizationFactor().*/
-  double   computeLogOfNormalizationFactor(unsigned int numSamples, bool updateFactorInternally) const;
-    
-  //! Updates the mean with the new value \c newLawExpVector.  
-  /*! This method deletes old expected values (allocated at construction or last call to this method).*/
-  void     updateLawExpVector(const V& newLawExpVector);
-  
-  //! Updates the lower triangular matrix from Cholesky decomposition of the covariance matrix to the new value \c newLowerCholLawCovMatrix.
-  /*! This method deletes old expected values (allocated at construction or last call to this method).*/
-  void     updateLawCovMatrix(const M& newLawCovMatrix);
-  
-  //! Returns the covariance matrix; access to protected attribute m_lawCovMatrix.  
-  const M& lawCovMatrix      () const;
 
-  //! Access to the vector of mean values and private attribute:  m_lawExpVector. 
-  const V& lawExpVector() const;
-  
-  //! Access to the vector of variance values and private attribute:  m_lawVarVector. 
-  const V& lawVarVector() const;
+  //! Actual value of the (transformed) Gaussian PDF
+  /*! This method calls lnValue() and applies the exponential to it.*/
+  double actualValue(const V & domainVector, const V * domainDirection,
+      V * gradVector, M * hessianMatrix, V * hessianEffect) const;
+
+  //! Logarithm of the value of the (transformed) Gaussian PDF (scalar function).
+  /*!
+   * The ln(value) comes from a summation of the Gaussian density:
+   *
+   * \f[
+   *   lnValue =- \sum_i \frac{1}{\sqrt{|covMatrix|} \sqrt{2 \pi}} exp(-\frac{(domainVector_i - lawExpVector_i) * covMatrix^{-1} *
+   *   (domainVector_i - lawExpVector_i) }{2} +
+   *   \log \text{logit}'(domainVector_i),
+   * \f]
+   *
+   * where the \f$ covMatrix \f$ may recovered via \c this->lawVarVector(), in
+   * case of diagonal matrices or via \c this->m_lawCovMatrix, otherwise.
+   */
+  double lnValue(const V & domainVector, const V * domainDirection,
+      V * gradVector, M * hessianMatrix, V * hessianEffect) const;
+
+  //! Computes the logarithm of the normalization factor.
+  /*!
+   * This routine calls BaseJointPdf::commonComputeLogOfNormalizationFactor().
+   */
+  double computeLogOfNormalizationFactor(unsigned int numSamples,
+      bool updateFactorInternally) const;
+
+  //! Updates the mean of the Gaussian (not transformed) with the new value \c newLawExpVector.
+  /*!
+   * This method deletes old expected values (allocated at construction or last
+   * call to this method).
+   */
+  void updateLawExpVector(const V & newLawExpVector);
+
+  //! Updates the lower triangular matrix from Cholesky decomposition of the covariance matrix to the new value \c newLowerCholLawCovMatrix.
+  /*!
+   * This method deletes old expected values (allocated at construction or
+   * last call to this method).
+   */
+  void updateLawCovMatrix(const M & newLawCovMatrix);
+
+  //! Returns the covariance matrix; access to protected attribute m_lawCovMatrix.
+  const M & lawCovMatrix() const;
+
+  //! Access to the vector of mean values of the Gaussian (not transformed) and private attribute:  m_lawExpVector.
+  const V & lawExpVector() const;
+
+  //! Access to the vector of variance values and private attribute:  m_lawVarVector.
+  const V & lawVarVector() const;
   //@}
 private:
   using BaseScalarFunction<V,M>::m_env;
@@ -106,10 +142,10 @@ private:
   using BaseScalarFunction<V,M>::m_domainSet;
   using BaseJointPdf<V,M>::m_normalizationStyle;
   using BaseJointPdf<V,M>::m_logOfNormalizationFactor;
-  V*       m_lawExpVector;
-  V*       m_lawVarVector;
-  bool     m_diagonalCovMatrix;
-  const M* m_lawCovMatrix;
+  V * m_lawExpVector;
+  V * m_lawVarVector;
+  bool m_diagonalCovMatrix;
+  const M * m_lawCovMatrix;
 
   const BoxSubset<V, M> & m_domainBoxSubset;
 };
