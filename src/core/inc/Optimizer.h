e39aff2ccd253ff4f95ece2fba8ef1b593e2fe8c
Damon McDougall <damon@ices.utexas.edu>
2014-09-05 16:01:18 -0500

    Add Optimizer base stubs

---
+++ src/core/inc/Optimizer.h
@@ -0,0 +1,54 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef UQ_BASE_OPTIMIZER_H
+#define UQ_BASE_OPTIMIZER_H
+
+namespace QUESO {
+
+/*!
+ * \file Optimizer.h
+ * \brief Class for handling optimization of scalar functions
+ *
+ * \class BaseOptimizer
+ * \brief A base class for handling optimisation of scalar functions
+ *
+ * WRITE DOCS HERE
+ */
+
+class BaseOptimizer {
+public:
+  //! @name Constructor/Destructor methods.
+  //@{
+  //! Default constructor.
+  BaseOptimizer();
+
+  //! Destructor
+  virtual ~BaseOptimizer();
+  //@}
+};
+
+}  // End namespace QUESO
+
+#endif // UQ_BASE_OPTIMIZER_H
