c5cfe2cea7ddc80a648d53dc7837ceaed90fe62d
Paul T. Bauman <ptbauman@gmail.com>
2014-10-16 07:04:42 -0400

    First draft of OptimizerMonitor
    Untested at this point.

---
+++ src/core/inc/OptimizerMonitor.h
@@ -0,0 +1,65 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef UQ_OPTIMIZER_MONITOR_H
+#define UQ_OPTIMIZER_MONITOR_H
+
+#include <vector>
+
+namespace QUESO
+{
+  //! Object to monitor convergence of optimizers
+  class OptimizerMonitor
+  {
+  public:
+
+    //! Constructor
+    /*!
+     * Input paramter n_iters will reserve space for monitor data
+     * for n_iters. This is for efficiency - if the number of iterations
+     * goes beyond n_iters, there will not be an error, just less performance
+     * by the monitor. Defaults to reserving space for 100 iterations
+     */
+    OptimizerMonitor( unsigned int n_iters = 100 );
+
+    ~OptimizerMonitor();
+
+    void set_display_output( bool enable_output, bool print_xmin );
+    
+    void append( std::vector<double>& x_min, double objective, double norm );
+    
+  private:
+    
+    bool m_display_conv;
+    bool m_print_xmin;
+
+    std::vector<std::vector<double> > m_minimizer_hist;
+    std::vector<double> m_objective_hist;
+    std::vector<double> m_norm_hist;
+
+  };
+
+} // end namespace QUESO
+
+#endif // UQ_OPTIMIZER_MONITOR_H
---
+++ src/core/src/OptimizerMonitor.C
@@ -0,0 +1,76 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008,2009,2010,2011,2012,2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#include <queso/OptimizerMonitor.h>
+
+namespace QUESO
+{
+
+  OptimizerMonitor::OptimizerMonitor( unsigned int n_iters )
+    : m_display_conv(false),
+      m_print_xmin(false)
+  {
+    m_minimizer_hist.reserve(n_iters);
+    m_objective_hist.reserve(n_iters);
+    m_norm_hist.reserve(n_iters);
+  }
+
+  OptimizerMonitor::~OptimizerMonitor()
+  {}
+
+  void OptimizerMonitor::set_display_output( bool enable_output, bool print_xmin )
+  {
+    m_display_conv = enable_output;
+    m_print_xmin = print_xmin;
+  }
+
+  void OptimizerMonitor::append( std::vector<double>& x_min, double objective, double norm )
+  {
+    m_minimizer_hist.push_back(x_min);
+    m_objective_hist.push_back(objective);
+    m_norm_hist.push_back(norm);
+
+    /*
+    if( m_display_conv )
+      {
+        if( m_minimizer_hist.size() == 1 )
+          {
+            unsigned int width = 29;
+            
+            printf("%5c",'i');
+            printf("%7s     ",'f');
+            printf("%8s    ",'norm');
+              
+            printf("%s\n", std::string(width,"-").c_str() );
+          }
+
+        printf("%5d %12.5e %12.5e\n", 
+               m_norm_hist.size(),
+               objective, 
+               norm );
+      }
+    */
+  }
+
+} // end namespace QUESO
