0ab825a2abeac9ea989e860e3c91429d80839c9a
Damon McDougall <damon@ices.utexas.edu>
2015-03-06 17:32:51 -0600

    Adding classes for Gaussian block llhd with random coeffs

---
+++ src/stats/inc/GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients.h
@@ -0,0 +1,83 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008-2015 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef UQ_GAUSSIAN_LIKELIHOOD_BLOCK_DIAG_COV_RAND_COEFFS_H
+#define UQ_GAUSSIAN_LIKELIHOOD_BLOCK_DIAG_COV_RAND_COEFFS_H
+
+#include <queso/GslBlockMatrix.h>
+#include <queso/GaussianLikelihood.h>
+
+namespace QUESO {
+
+/*!
+ * \file GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients.h
+ *
+ * \class GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients
+ * \brief A class representing a Gaussian likelihood with block-diagonal covariance matrix
+ */
+
+template<class V, class M>
+class GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients : public BaseGaussianLikelihood<V, M> {
+public:
+  //! @name Constructor/Destructor methods.
+  //@{
+  //! Default constructor.
+  /*!
+   * Instantiates a Gaussian likelihood function, given a prefix, its domain, a
+   * vector of observations and a block diagonal covariance matrix.
+   * The diagonal covariance matrix is of type \c GslBlockMatrix.  Each block
+   * in the block diagonal matrix is an object of type \c GslMatrix.
+   */
+  GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients(const char * prefix,
+      const VectorSet<V, M> & domainSet, const V & observations,
+      const GslBlockMatrix & covariance);
+
+  //! Destructor
+  virtual ~GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients();
+  //@}
+
+  //! Actual value of the scalar function.
+  virtual double actualValue(const V & domainVector, const V * domainDirection,
+      V * gradVector, M * hessianMatrix, V * hessianEffect) const;
+
+  //! Logarithm of the value of the scalar function.
+  /*!
+   * The last \c n elements of \c domainVector, where \c n is the number of
+   * blocks in the block diagonal covariance matrix, are treated as
+   * hyperparameters and will be sample in a statistical inversion.
+   *
+   * The user need not concern themselves with handling these in the model
+   * evaluation routine, since they are handled by the likelihood evaluation
+   * routine.
+   */
+  virtual double lnValue(const V & domainVector, const V * domainDirection,
+      V * gradVector, M * hessianMatrix, V * hessianEffect) const;
+
+private:
+  const GslBlockMatrix & m_covariance;
+};
+
+}  // End namespace QUESO
+
+#endif  // UQ_GAUSSIAN_LIKELIHOOD_BLOCK_DIAG_COV_RAND_COEFFS_H
