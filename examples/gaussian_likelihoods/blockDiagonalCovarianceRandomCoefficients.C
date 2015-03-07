1b50e2a7853013e0d3675cfbb65d027388587a09
Damon McDougall <damon@ices.utexas.edu>
2015-03-06 18:08:21 -0600

    Adding example for block random coefficients

---
+++ examples/gaussian_likelihoods/blockDiagonalCovarianceRandomCoefficients.C
@@ -0,0 +1,133 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008-2015 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#include <queso/GslVector.h>
+#include <queso/GslMatrix.h>
+#include <queso/GslBlockMatrix.h>
+#include <queso/VectorSet.h>
+#include <queso/BoxSubset.h>
+#include <queso/UniformVectorRV.h>
+#include <queso/GenericVectorRV.h>
+#include <queso/GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients.h>
+#include <queso/StatisticalInverseProblem.h>
+
+template<class V, class M>
+class Likelihood : public QUESO::GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients<V, M>
+{
+public:
+
+  Likelihood(const char * prefix, const QUESO::VectorSet<V, M> & domain,
+      const V & observations, const QUESO::GslBlockMatrix & covariance)
+    : QUESO::GaussianLikelihoodBlockDiagonalCovarianceRandomCoefficients<V, M>(prefix, domain,
+        observations, covariance)
+  {
+  }
+
+  virtual ~Likelihood()
+  {
+  }
+
+  virtual void evaluateModel(const V & domainVector, const V * domainDirection,
+      V & modelOutput, V * gradVector, M * hessianMatrix,
+      V * hessianEffect) const
+  {
+    // Evaluate model and fill up the m_modelOutput member variable
+    for (unsigned int i = 0; i < modelOutput.sizeLocal(); i++) {
+      modelOutput[i] = 1.0;
+    }
+  }
+};
+
+int main(int argc, char ** argv) {
+  MPI_Init(&argc, &argv);
+
+  QUESO::FullEnvironment env(MPI_COMM_WORLD, argv[1], "", NULL);
+
+  // Need 3 dims because two are for the hyperparameters
+  QUESO::VectorSpace<QUESO::GslVector, QUESO::GslMatrix> paramSpace(env,
+      "param_", 3, NULL);
+
+  double min_val = 0.0;
+  double max_val = INFINITY;
+
+  QUESO::GslVector paramMins(paramSpace.zeroVector());
+  QUESO::GslVector paramMaxs(paramSpace.zeroVector());
+
+  // Hyperparameter bounds
+  paramMins.cwSet(min_val);
+  paramMaxs.cwSet(max_val);
+
+  // Model parameter bounds
+  paramMaxs[0] = 1.0;
+
+  QUESO::BoxSubset<QUESO::GslVector, QUESO::GslMatrix> paramDomain("param_",
+      paramSpace, paramMins, paramMaxs);
+
+  QUESO::UniformVectorRV<QUESO::GslVector, QUESO::GslMatrix> priorRv("prior_",
+      paramDomain);
+
+  // Set up observation space
+  QUESO::VectorSpace<QUESO::GslVector, QUESO::GslMatrix> obsSpace(env,
+      "obs_", 3, NULL);
+
+  // Fill up observation vector
+  QUESO::GslVector observations(obsSpace.zeroVector());
+  observations[0] = 1.0;
+  observations[1] = 1.0;
+  observations[2] = 1.0;
+
+  // Set up block sizes for observation covariance matrix
+  std::vector<unsigned int> blockSizes(2);
+  blockSizes[0] = 1;  // First block is 1x1 (scalar)
+  blockSizes[1] = 2;  // Second block is 2x2
+
+  // Set up block matrix (identity matrix) with specified block sizes
+  QUESO::GslBlockMatrix covariance(blockSizes, observations, 1.0);
+
+  // Pass in observations to Gaussian likelihood object
+  Likelihood<QUESO::GslVector, QUESO::GslMatrix> lhood("llhd_", paramDomain,
+      observations, covariance);
+
+  QUESO::GenericVectorRV<QUESO::GslVector, QUESO::GslMatrix>
+    postRv("post_", paramSpace);
+
+  QUESO::StatisticalInverseProblem<QUESO::GslVector, QUESO::GslMatrix>
+    ip("", NULL, priorRv, lhood, postRv);
+
+  QUESO::GslVector paramInitials(paramSpace.zeroVector());
+
+  paramInitials[0] = 0.0;
+
+  QUESO::GslMatrix proposalCovMatrix(paramSpace.zeroVector());
+
+  for (unsigned int i = 0; i < 1; i++) {
+    proposalCovMatrix(i, i) = 0.1;
+  }
+
+  ip.solveWithBayesMetropolisHastings(NULL, paramInitials, &proposalCovMatrix);
+
+  MPI_Finalize();
+
+  return 0;
+}
