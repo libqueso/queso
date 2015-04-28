e083ac68ccf7289c1885845dc12a19f4779d3d69
Paul T. Bauman <ptbauman@gmail.com>
2015-04-28 14:45:29 -0400

    Test InterpolationSurrogateBuilder::build_values
    Use the 4D case and test that the resulting interpolating surrogate
    gives the right interpolant values after being setup by the builder.

--- test/test_InterpolationSurrogate/test_build_InterpolationSurrogateBuilder.C
+++ test/test_InterpolationSurrogate/test_build_InterpolationSurrogateBuilder.C
@@ -28,6 +28,8 @@
 #include <queso/LinearLagrangeInterpolationSurrogate.h>
 #include <queso/InterpolationSurrogateBuilder.h>
 
+double four_d_fn( double x, double y, double z, double a );
+
 template<class V, class M>
 class MyInterpolationBuilder : public QUESO::InterpolationSurrogateBuilder<V,M>
 {
@@ -39,8 +41,8 @@ public:
   virtual ~MyInterpolationBuilder(){};
 
   virtual double evaluate_model( const V & domainVector ) const
-  { queso_not_implemented();
-    return 0.0; };
+  { queso_assert_equal_to( domainVector.sizeGlobal(), 4);
+    return four_d_fn(domainVector[0],domainVector[1],domainVector[2],domainVector[3]); };
 };
 
 int main(int argc, char ** argv)
@@ -51,20 +53,28 @@ int main(int argc, char ** argv)
   int return_flag = 0;
 
   QUESO::VectorSpace<QUESO::GslVector, QUESO::GslMatrix>
-    paramSpace(env,"param_", 1, NULL);
-
-  double min_val = -5;
-  double max_val = 3;
+    paramSpace(env,"param_", 4, NULL);
 
   QUESO::GslVector paramMins(paramSpace.zeroVector());
-  paramMins.cwSet(min_val);
+  paramMins[0] = -1;
+  paramMins[1] = -0.5;
+  paramMins[2] = 1.1;
+  paramMins[3] = -2.1;
+
   QUESO::GslVector paramMaxs(paramSpace.zeroVector());
-  paramMaxs.cwSet(max_val);
+  paramMaxs[0] = 0.9;
+  paramMaxs[1] = 3.14;
+  paramMaxs[2] = 2.1;
+  paramMaxs[3] = 4.1;
 
   QUESO::BoxSubset<QUESO::GslVector, QUESO::GslMatrix>
     paramDomain("param_", paramSpace, paramMins, paramMaxs);
 
-  std::vector<unsigned int> n_points(1, 101);
+  std::vector<unsigned int> n_points(4);
+  n_points[0] = 101;
+  n_points[1] = 51;
+  n_points[2] = 31;
+  n_points[3] = 41;
 
   QUESO::InterpolationSurrogateData<QUESO::GslVector, QUESO::GslMatrix>
     data(paramDomain,n_points);
@@ -72,8 +82,44 @@ int main(int argc, char ** argv)
   MyInterpolationBuilder<QUESO::GslVector,QUESO::GslMatrix>
     builder( data );
 
+  builder.build_values();
+
   QUESO::LinearLagrangeInterpolationSurrogate<QUESO::GslVector,QUESO::GslMatrix>
-    one_d_surrogate( data );
+    four_d_surrogate( data );
+
+  QUESO::GslVector domainVector(paramSpace.zeroVector());
+  domainVector[0] = -0.4;
+  domainVector[1] = 3.0;
+  domainVector[2] = 1.5;
+  domainVector[3] = 1.65;
+
+  double test_val = four_d_surrogate.evaluate(domainVector);
+
+  double exact_val = four_d_fn(domainVector[0],domainVector[1],domainVector[2],domainVector[3]);
+
+  double tol = 2.0*std::numeric_limits<double>::epsilon();
+
+  double rel_error = (test_val - exact_val)/exact_val;
+
+  if( std::fabs(rel_error) > tol )
+    {
+      std::cerr << "ERROR: Tolerance exceeded for 4D Lagrange interpolation test."
+                << std::endl
+                << " test_val  = " << test_val << std::endl
+                << " exact_val = " << exact_val << std::endl
+                << " rel_error = " << rel_error << std::endl
+                << " tol       = " << tol << std::endl;
+
+      return_flag = 1;
+    }
 
   return return_flag;
+}
+
+double four_d_fn( double x, double y, double z, double a )
+{
+  return 3.0 + 2.5*x - 3.1*y + 2.71*z + 3.14*a
+    + 0.1*x*y + 1.2*x*z + 0.5*y*z + 0.1*x*a + 1.1*y*a + 2.1*z*a
+    + 0.3*x*y*a + 0.5*x*z*a + 1.2*y*z*a + 0.9*x*y*z
+    + 2.5*x*y*z*a;
 }
