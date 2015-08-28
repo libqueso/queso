43fd1bed721d816445c027700013ac12822a7d51
Damon McDougall <damon@ices.utexas.edu>
2015-08-28 15:54:26 -0500

    Add SharedPtr

---
+++ test/pointers/test_SharedPtr.C
@@ -0,0 +1,39 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// QUESO - a library to support the Quantification of Uncertainty
+// for Estimation, Simulation and Optimization
+//
+// Copyright (C) 2008-2015 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#include <queso/asserts.h>
+#include <queso/SharedPtr.h>
+
+int main(int argc, char* argv[])
+{
+  QUESO::SharedPtr<int>::Type int_ptr1(new int(1));
+
+  QUESO::SharedPtr<int>::Type int_ptr2(int_ptr1);
+
+  *int_ptr2 = 2;
+
+  queso_require_equal_to_msg(*int_ptr1, 2, "pointed-to values are not equal!");
+
+  return 0;
+}
