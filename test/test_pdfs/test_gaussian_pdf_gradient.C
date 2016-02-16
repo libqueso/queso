411e7cb5514f3f944fd312fc067223ad1ea2a093
Damon McDougall <damon@ices.utexas.edu>
2016-02-16 14:05:02 -0600

    Adding another (analytical) gradient test for the Gaussian

--- test/test_pdfs/test_gaussian_pdf_gradient.C
+++ test/test_pdfs/test_gaussian_pdf_gradient.C
@@ -24,6 +24,7 @@
 
 #include <queso/Environment.h>
 #include <queso/GslVector.h>
+#include <queso/GslMatrix.h>
 #include <queso/GaussianJointPdf.h>
 
 #define TOL 1e-15
@@ -50,28 +51,58 @@ int main(int argc, char ** argv)
   QUESO::BoxSubset<> paramDomain("param_", paramSpace, paramMins, paramMaxs);
 
   QUESO::GslVector mean(paramSpace.zeroVector());
-  QUESO::GslVector var(paramSpace.zeroVector());
+  QUESO::GslMatrix var(paramSpace.zeroVector());
   mean[0] = 2.0;
   mean[1] = 3.0;
   mean[2] = 4.0;
-  var[0] = 5.0;
-  var[1] = 6.0;
-  var[2] = 7.0;
+  var(0,0) = 5.0;
+  var(1,1) = 6.0;
+  var(2,2) = 7.0;
 
+  // Check the zero-gradient case
   QUESO::GaussianJointPdf<> pdf("", paramDomain, mean, var);
 
   QUESO::GslVector lnGradVector(paramSpace.zeroVector());
   QUESO::GslVector gradVector(paramSpace.zeroVector());
+  QUESO::GslVector point(mean);
 
-  double lnPdfValue1 = pdf.lnValue(mean, NULL, &lnGradVector, NULL, NULL);
+  double lnPdfValue1 = pdf.lnValue(point, NULL, &lnGradVector, NULL, NULL);
   queso_require_less_equal_msg(std::abs(lnGradVector[0]), TOL, "grad log gaussian pdf values are incorrect");
   queso_require_less_equal_msg(std::abs(lnGradVector[1]), TOL, "grad log gaussian pdf values are incorrect");
   queso_require_less_equal_msg(std::abs(lnGradVector[2]), TOL, "grad log gaussian pdf values are incorrect");
 
-  double pdfValue1 = pdf.actualValue(mean, NULL, &gradVector, NULL, NULL);
+  double pdfValue1 = pdf.actualValue(point, NULL, &gradVector, NULL, NULL);
   queso_require_less_equal_msg(std::abs(gradVector[0]), TOL, "grad guassian pdf values are incorrect");
   queso_require_less_equal_msg(std::abs(gradVector[1]), TOL, "grad guassian pdf values are incorrect");
   queso_require_less_equal_msg(std::abs(gradVector[2]), TOL, "grad guassian pdf values are incorrect");
+
+
+  // Check the 1-gradient case (in log space)
+  mean[0] = 0.0;
+  mean[1] = 0.0;
+  mean[2] = 0.0;
+
+  var(0,0) = 1.0;
+  var(0,1) = 0.8;
+  var(0,2) = 0.7;
+  var(1,0) = 0.8;
+  var(1,1) = 2.0;
+  var(1,2) = 0.6;
+  var(2,0) = 0.7;
+  var(2,1) = 0.6;
+  var(2,2) = 3.0;
+
+  point[0] = -2.5;
+  point[1] = -3.4;
+  point[2] = -4.3;
+
+  QUESO::GaussianJointPdf<> pdf2("", paramDomain, mean, var);
+
+  lnPdfValue1 = pdf2.lnValue(point, NULL, &lnGradVector, NULL, NULL);
+
+  queso_require_less_equal_msg(std::abs(lnGradVector[0] - 1.0), TOL, "grad log gaussian pdf2 values are incorrect");
+  queso_require_less_equal_msg(std::abs(lnGradVector[1] - 1.0), TOL, "grad log gaussian pdf2 values are incorrect");
+  queso_require_less_equal_msg(std::abs(lnGradVector[2] - 1.0), TOL, "grad log gaussian pdf2 values are incorrect");
 
 #ifdef QUESO_HAS_MPI
   MPI_Finalize();
