8b2357fd0706b077cf9c6c4c1828cb490f42f611
Paul T. Bauman <ptbauman@gmail.com>
2017-01-16 16:26:21 -0500

    Add Legendre 1D quadrature unit test
    This test makes sure integrate polynominals of the highest allowable
    order exactly to within machine tolerance (except 7th order which
    for some reason only has 8 digits in the rule). We do this for both
    the unscaled and rescaled intervals.

--- test/unit/quadrature_1d.C
+++ test/unit/quadrature_1d.C
@@ -31,6 +31,9 @@
 
 #include <queso/1DQuadrature.h>
 
+#include <cmath>
+#include <limits>
+
 namespace QUESOTesting
 {
   class Quadrature1DTestBase : public CppUnit::TestCase
@@ -63,6 +66,117 @@ namespace QUESOTesting
       CPPUNIT_ASSERT_DOUBLES_EQUAL( 0.0, rel_error, tol );
     }
   };
+
+  class LegendreQuadrature1DTest : public Quadrature1DTestBase
+  {
+  public:
+    CPPUNIT_TEST_SUITE( LegendreQuadrature1DTest );
+
+    CPPUNIT_TEST( test_1d_legendre_quadrature_unscaled_interval );
+    CPPUNIT_TEST( test_1d_legendre_quadrature_scaled_interval );
+
+    CPPUNIT_TEST_SUITE_END();
+
+    // yes, this is necessary
+  public:
+
+    void test_1d_legendre_quadrature_unscaled_interval()
+    {
+      // This test only checks on the interval \pm 1
+      double min_domain_value=-1.0;
+      double max_domain_value=1.0;
+
+      this->test_1d_legendre_quadrature(min_domain_value,max_domain_value);
+    }
+
+    void test_1d_legendre_quadrature_scaled_interval()
+    {
+      // This test only checks on the interval \pm 1
+      double min_domain_value=-2.7183;
+      double max_domain_value=3.1415;
+
+      this->test_1d_legendre_quadrature(min_domain_value,max_domain_value);
+    }
+
+    // e.g. 4*x^3 + 3*x^2 + 2*x + 1
+    virtual double f( double x )
+    {
+      CPPUNIT_ASSERT(_order>=0);
+      double value = 0.0;
+
+      for( int i = 0; i <= _order; i++ )
+        value += (i+1)*std::pow( x, i );
+
+      return value;
+    }
+
+    // Integral of the function in f()
+    virtual double int_f( double lower, double upper )
+    {
+      CPPUNIT_ASSERT(_order>=0);
+      double value = 0.0;
+
+      for( int i = 0; i <= _order; i++ )
+        value += (std::pow(upper,i+1.0) - std::pow(lower,i+1.0) );
+
+      return value;
+    }
+
+    void setUp()
+    {
+      // Setup for error checking, in order to make sure this gets reset later.
+      _order = -1;
+    }
+
+  private:
+
+    int _order;
+
+    void testing_orders( std::vector<unsigned int> & orders )
+    {
+      // These are the valid listed orders for Legendre quadrature in QUESO;
+      // TODO: With C++11, we can initialize this with array syntax
+      orders.resize(11);
+      orders[0] = 1;
+      orders[1] = 2;
+      orders[2] = 3;
+      orders[3] = 4;
+      orders[4] = 5;
+      orders[5] = 6;
+      orders[6] = 7;
+      orders[7] = 10;
+      orders[8] = 11;
+      orders[9] = 12;
+      orders[10] = 16;
+    }
+
+    void test_1d_legendre_quadrature(double min_domain_value, double max_domain_value)
+    {
+      std::vector<unsigned int> testing_orders;
+      this->testing_orders( testing_orders );
+
+      for( std::vector<unsigned int>::const_iterator it = testing_orders.begin();
+           it < testing_orders.end(); ++it )
+      {
+        unsigned int quad_order = *it;
+        QUESO::UniformLegendre1DQuadrature quad_rule(min_domain_value,max_domain_value,quad_order,false);
+
+        // We should be able to integrate polynomials of order 2*n-1 exactly
+        // order = quad_order + 1 by QUESO convention
+        _order = 2*(quad_rule.order()+1)-1;
+
+        double tol = std::numeric_limits<double>::epsilon()*20;
+
+        // 7th order quadrature only has positions/weights out to 8 digits...
+        if( quad_order == 7 )
+          tol = 1.0e-7;
+
+        this->test_quadrature_rule( quad_rule, min_domain_value, max_domain_value, tol );
+      }
+    }
+  };
+
+  CPPUNIT_TEST_SUITE_REGISTRATION( LegendreQuadrature1DTest );
 
 } // end namespace QUESOTesting
 
