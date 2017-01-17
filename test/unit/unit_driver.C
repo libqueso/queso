f279960a04db85904a1fb5948d1a6971b31ccb30
Paul T. Bauman <ptbauman@gmail.com>
2017-01-17 18:39:15 -0500

    Update License to 2017

--- test/unit/unit_driver.C
+++ test/unit/unit_driver.C
@@ -4,7 +4,7 @@
 // QUESO - a library to support the Quantification of Uncertainty
 // for Estimation, Simulation and Optimization
 //
-// Copyright (C) 2008-2015 The PECOS Development Team
+// Copyright (C) 2008-2017 The PECOS Development Team
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the Version 2.1 GNU Lesser General
