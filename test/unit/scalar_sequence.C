5c92182719ca0bfc2d143ffb2d90868ca33b279e
Damon McDougall <damon@ices.utexas.edu>
2017-01-23 12:59:05 -0600

    Add unifiedMinPlain test

--- test/unit/scalar_sequence.C
+++ test/unit/scalar_sequence.C
@@ -44,6 +44,7 @@ public:
   CPPUNIT_TEST(test_setup);
   CPPUNIT_TEST(test_erase);
   CPPUNIT_TEST(test_sub_min_plain);
+  CPPUNIT_TEST(test_unified_min_plain);
   CPPUNIT_TEST_SUITE_END();
 
   // yes, this is necessary
@@ -99,6 +100,18 @@ public:
     double subMinPlain = sequence->subMinPlain();
 
     CPPUNIT_ASSERT_EQUAL(0.0, subMinPlain);
+  }
+
+  void test_unified_min_plain()
+  {
+    // Set *sequence to (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+    for (unsigned int i = 0; i < 13; i++) {
+      (*sequence)[i] = i;
+    }
+
+    double unifiedMinPlain = sequence->unifiedMinPlain(false);
+
+    CPPUNIT_ASSERT_EQUAL(0.0, unifiedMinPlain);
   }
 
 private:
