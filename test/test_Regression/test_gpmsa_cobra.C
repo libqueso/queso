c0463bf6789a6c1bed428737ae808631465c76d7
Roy H. Stogner <roystgnr@ices.utexas.edu>
2017-04-14 15:29:19 -0500

    One more fgets return value check

--- test/test_Regression/test_gpmsa_cobra.C
+++ test/test_Regression/test_gpmsa_cobra.C
@@ -36,7 +36,9 @@ double readData(const std::vector<QUESO::SharedPtr<QUESO::GslVector>::Type> & si
   double m2sim = 0;
 
   // First line is a header, so we ignore it
-  fgets(line, size, fp_in);
+  char * gotline = fgets(line, size, fp_in);
+  if (!gotline)
+    queso_error_msg("dakota_pstudy.dat was unreadable");
 
   i = 0;
   while (fscanf(fp_in, "%d %lf %lf %lf %lf %lf %lf\n", &id, &k_tmasl, &k_tmoml,
