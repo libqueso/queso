00692627c090c9e99e0429b66129f8de8b6615b3
Damon McDougall <damon@ices.utexas.edu>
2017-05-24 16:03:59 -0500

    Make array size compile-time constant

--- examples/gp/vector/gpmsa_vector.C
+++ examples/gp/vector/gpmsa_vector.C
@@ -8,6 +8,8 @@
 
 #include <cstdio>
 
+#define LINE_SIZE 512
+
 // Read in data files.  Return std deviations for each output.
 std::vector<double>
 readData(const std::vector<QUESO::SharedPtr<QUESO::GslVector>::Type> & simulationScenarios,
@@ -21,9 +23,9 @@ readData(const std::vector<QUESO::SharedPtr<QUESO::GslVector>::Type> & simulatio
   if (!fp_in)
     queso_error_msg("Cannot find dakota_pstudy.dat");
 
-  unsigned int i, id, size = 512;
+  unsigned int i, id;
   double k_tmasl, k_tmoml, k_tnrgl, k_xkwlx, k_cd, pressure;
-  char line[size];
+  char line[LINE_SIZE];
 
   // The user should know what the bounds on the data are
   double mins[] = {0.95, 0.9, 0.9, 0.9, 0.9};
@@ -36,7 +38,7 @@ readData(const std::vector<QUESO::SharedPtr<QUESO::GslVector>::Type> & simulatio
   double m2expsim = 0;
 
   // First line is a header, so we ignore it
-  char * gotline = fgets(line, size, fp_in);
+  char * gotline = fgets(line, LINE_SIZE, fp_in);
   if (!gotline)
     queso_error_msg("dakota_pstudy.dat was unreadable");
 
